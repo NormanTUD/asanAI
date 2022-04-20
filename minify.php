0b1cb8fe79ed454760ceba2f683de470378fb19c
Norman Koch <kochnorman31@gmail.com>
2022-04-20 16:29:38 +0200

    added cache control pragma

--- minify.php
+++ minify.php
@@ -3,9 +3,11 @@
 
 	if(file_exists($file)) {
 		if(preg_match("/^[\/\.a-zA-Z0-9-_]+\.js$/", $file)) {
+			header('Cache-Control: max-age=86400');
 			header("Content-Type: application/javascript");
 			system("./minify/minify/bin/minifyjs $file");
 		} else if(preg_match("/^[\/\.a-zA-Z0-9-_]+\.css$/", $file)) {
+			header('Cache-Control: max-age=86400');
 			header("Content-Type: text/css");
 			$css = file_get_contents($file);
 			$css = preg_replace(
