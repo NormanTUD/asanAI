a914d960bc64085ac223d40c3eccfd04b5f8bd3b
Norman Koch <kochnorman31@gmail.com>
2022-04-20 07:51:41 +0200

    Local version of MathJax

--- index.php
+++ index.php
@@ -126,7 +126,9 @@
 		</script>
 
 		<!-- mathjax -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+		<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>-->
+		<script type="text/javascript" src="MathJax-2.7.7/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+		
 
 		<script type="text/x-mathjax-config">
 			var font = "Neo-Euler";
