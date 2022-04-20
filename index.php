e62c41be942e5a6d39ae3041551d30e5305229e7
Norman Koch <kochnorman31@gmail.com>
2022-04-20 16:31:21 +0200

    removed dier from index.php

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php
 	include("functions.php");
 	
-	function dier ($data, $enable_html = 0) {
-		$source_data = debug_backtrace()[0];
-		@$source = 'Aufgerufen von <b>'.debug_backtrace()[1]['file'].'</b>::<i>'.debug_backtrace()[1]['function'].'</i>, line '.htmlentities($source_data['line'])."<br>\n";
-		print $source;
-
-		print "<pre>\n";
-		ob_start();
-		print_r($data);
-		$buffer = ob_get_clean();
-		if($enable_html) {
-			print $buffer;
-		} else {
-			print htmlentities($buffer);
-		}
-		print "</pre>\n";
-
-		print "Backtrace:\n";
-		print "<pre>\n";
-		foreach (debug_backtrace() as $trace) {
-			print htmlentities(sprintf("\n%s:%s %s", $trace['file'], $trace['line'], $trace['function']));
-		}
-		print "</pre>\n";
-
-		exit();
-	}
-
 	$darkmode = 0;
 
 	if(array_key_exists("darkmode", $_COOKIE) && $_COOKIE["darkmode"]) {
