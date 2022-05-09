8145dbb00d3b976ef01a054379efc35da2c804ae
Norman Koch <kochnorman31@gmail.com>
2022-05-09 09:57:25 +0200

    fixed typo

--- predict.js
+++ predict.js
@@ -203,7 +203,7 @@ async function predict (item, force_category, dont_write_to_predict_tab) {
 }
 
 function show_prediction (keep_show_after_training_hidden, dont_go_to_tab) {
-	$(".sehow_when_predicting").show();
+	$(".show_when_predicting").show();
 
 	hide_unused_layer_visualization_headers();
 
