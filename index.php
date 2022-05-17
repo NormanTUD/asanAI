292792e0eaecb6931ce3e1313368e9def146ad68
Norman Koch <kochnorman31@gmail.com>
2022-05-17 10:05:28 +0200

    minor cosmetic changes

--- index.php
+++ index.php
@@ -761,8 +761,8 @@ $GLOBALS['minify'] = 0;
 									<li><a id="code_tab_label" href="#code_tab" data-intro="Shows Python/NodeJS/TensorFlow.js-HTML-Code of the currently configured neural network.">Code</a></li>
 									<li><a href="#summary_tab" data-intro="Shows the model.summary of the currently configured model">Summary</a></li>
 									<li><a id="training_data_tab_label" href="#training_data_tab">Data</a></li>
-									<li><a id="own_image_data_label" href="#own_image_data">Own image data</a></li>
-									<li><a id="own_tensor_data_label" href="#own_tensor_data">Own tensor data</a></li>
+									<li><a id="own_image_data_label" href="#own_image_data">Own images</a></li>
+									<li><a id="own_tensor_data_label" href="#own_tensor_data">Own tensors</a></li>
 									<li><a id="own_csv_data_label" href="#own_csv_data">Own CSV data</a></li>
 									<li><a id="tfvis_tab_label" href="#tfvis_tab" data-intro="Shows the training data (if possible) and the training progress.">Training</a></li>
 									<li id="predict_tab_label"><a href="#predict_tab" data-intro="Allows you to predict data from the trained model.">Predict</a></li>
