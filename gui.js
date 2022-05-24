764423e5fc97cfc7ebe15ffb123cc8b7b1b8c9cf
Norman Koch <kochnorman31@gmail.com>
2022-05-24 09:34:57 +0200

    added js cookie functions

--- gui.js
+++ gui.js
@@ -4126,3 +4126,28 @@ function add_loss_functions_to_plotly_visualizer(data) {
 	create_plotly_table();
 	plotly_show_loss_graph();
 }
+
+function setCookie(name,value,days) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
