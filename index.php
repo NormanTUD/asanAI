be19a9bc92995334095ee4a6e8434e1764143a91
Norman Koch <kochnorman31@gmail.com>
2022-05-24 10:33:31 +0200

    added test code

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 			var show_layer_trial_error = <?php print array_key_exists("show_layer_trial_error", $_GET) ? 1 : 0; ?>;
 		</script>
 
-
+		<link rel="stylesheet" href="./wizard_style.css">
 <?php
 		$tablist_color = "254c87";
 
@@ -700,6 +700,158 @@ $GLOBALS['minify'] = 0;
 			</div>
 
 			<div id="maindiv">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<div class="content">
+			<div class="content__inner">
+				<div class="container overflow-hidden">
+					<div class="multisteps-form">
+						<div class="row">
+							<div class="col-12 col-lg-8 ml-auto mr-auto mb-4">
+								<div class="multisteps-form__progress">
+									<button class="multisteps-form__progress-btn js-active" type="button" title="User Info">User Info</button>
+									<button class="multisteps-form__progress-btn" type="button" title="Order Info">Order Info</button>
+									<button class="multisteps-form__progress-btn" type="button" title="Comments">Comments        </button>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-8 m-auto">
+								<form class="multisteps-form__form">
+									<div class="multisteps-form__panel shadow p-4 rounded bg-white js-active" data-animation="scaleIn">
+										<h3 class="multisteps-form__title">Your User Info</h3>
+										<div class="multisteps-form__content">
+											<div class="form-row mt-4">
+												<div class="col-12 col-sm-6">
+													<input class="multisteps-form__input form-control" type="text" placeholder="First Name"/>
+												</div>
+												<div class="col-12 col-sm-6 mt-4 mt-sm-0">
+													<input class="multisteps-form__input form-control" type="text" placeholder="Last Name"/>
+												</div>
+											</div>
+											<div class="form-row mt-4">
+												<div class="col-12 col-sm-6">
+													<input class="multisteps-form__input form-control" type="text" placeholder="Login"/>
+												</div>
+												<div class="col-12 col-sm-6 mt-4 mt-sm-0">
+													<input class="multisteps-form__input form-control" type="email" placeholder="Email"/>
+												</div>
+											</div>
+											<div class="form-row mt-4">
+												<div class="col-12 col-sm-6">
+													<input class="multisteps-form__input form-control" type="password" placeholder="Password"/>
+												</div>
+												<div class="col-12 col-sm-6 mt-4 mt-sm-0">
+													<input class="multisteps-form__input form-control" type="password" placeholder="Repeat Password"/>
+												</div>
+											</div>
+											<div class="button-row d-flex mt-4">
+												<button class="btn btn-primary ml-auto js-btn-next" type="button" title="Next">Next</button>
+											</div>
+										</div>
+									</div>
+									<div class="multisteps-form__panel shadow p-4 rounded bg-white" data-animation="scaleIn">
+										<h3 class="multisteps-form__title">Your Order Info</h3>
+										<div class="multisteps-form__content">
+											<div class="row">
+												<div class="col-12 col-md-6 mt-4">
+													<div class="card shadow-sm">
+														<div class="card-body">
+															<h5 class="card-title">Item Title</h5>
+															<p class="card-text">Small and nice item description</p><a class="btn btn-primary" href="#" title="Item Link">Item Link</a>
+														</div>
+													</div>
+												</div>
+												<div class="col-12 col-md-6 mt-4">
+													<div class="card shadow-sm">
+														<div class="card-body">
+															<h5 class="card-title">Item Title</h5>
+															<p class="card-text">Small and nice item description</p><a class="btn btn-primary" href="#" title="Item Link">Item Link</a>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+												<div class="button-row d-flex mt-4 col-12">
+													<button class="btn btn-primary js-btn-prev" type="button" title="Prev">Prev</button>
+													<button class="btn btn-primary ml-auto js-btn-next" type="button" title="Next">Next</button>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="multisteps-form__panel shadow p-4 rounded bg-white" data-animation="scaleIn">
+										<h3 class="multisteps-form__title">Additional Comments</h3>
+										<div class="multisteps-form__content">
+											<div class="form-row mt-4">
+												<textarea class="multisteps-form__textarea form-control" placeholder="Additional Comments and Requirements"></textarea>
+											</div>
+											<div class="button-row d-flex mt-4">
+												<button class="btn btn-primary js-btn-prev" type="button" title="Prev">Prev</button>
+												<button class="btn btn-success ml-auto" type="button" title="Send">Send</button>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 				<div id="losses_popup" style="display: none">
 					<div class="popup_body less_transparent_glass_box">
 						<div id="table_div"></div>
@@ -1677,5 +1829,6 @@ $GLOBALS['minify'] = 0;
 
 			resize_window();
 		</script>
+		<script src="./wizard_script.js"></script>
 	</body>
 </html>
