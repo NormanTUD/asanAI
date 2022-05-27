5d8c04900517a20fce66e8a6b1acbd7d40691a0d
cloud1160 <claudia.adler@tu-dresden.de>
2022-05-27 10:23:35 +0200

    get_model_from_db

---
+++ get_model_from_db.php
@@ -0,0 +1,38 @@
+<?php 
+    include('functions.php');
+
+    if(array_key_exists("session_id", $_COOKIE)) {
+        
+        $user = get_user_id_from_session_id($_COOKIE["session_id"]);
+        if(is_null($user)) {
+            print "User doesn't exist.";
+        } else {
+            /*
+            db.models.find(
+                {
+                    $or: [{user: {$eq: 62}}, {is_public: {$eq: "true"}}]
+                }, 
+                
+                
+                {"user": true, "is_public": true, "category": true, "model_weights": true, "model_structure": true});
+            */
+            $filters = array(
+                '$or' => array(
+                    array("user" => array('$eq' => $user)),
+                    array("is_public" => array('$eq' => 'true'))
+                )
+                );
+            $options = array(
+                'user' => true,
+                'is_public' => true,
+                'category' => true,
+                'model_weights' => true,
+                'model_structure' => true
+            );
+
+            dier(find_mongo("tfd.models", $filters, $options));
+        }
+    } else {
+        print "You are not logged in.";
+    }
+?>
