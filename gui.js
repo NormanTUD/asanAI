77031138c6b30a3f68d4a2ba2e2d4e9efc95ed3e
Norman Koch <kochnorman31@gmail.com>
2022-06-01 15:37:40 +0200

    fixed cookie stuff

--- gui.js
+++ gui.js
@@ -4282,7 +4282,7 @@ function setCookie(name,value,days) {
 		date.setTime(date.getTime() + (days*24*60*60*1000));
 		expires = "; expires=" + date.toUTCString();
 	}
-	document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+	document.cookie = name + "=" + (value || "")  + expires + "; path=/; samesite=lax";
 }
 
 function getCookie(name) {
@@ -4297,7 +4297,7 @@ function getCookie(name) {
 }
 
 function eraseCookie(name) {
-	document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; samesite=lax';
 }
 
 function copy_options () {
