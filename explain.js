4f1ed36be795cf337f5cf85af56b0d683591a490
Norman Koch <kochnorman31@gmail.com>
2022-06-30 14:41:04 +0200

    empty output shapes get red

--- explain.js
+++ explain.js
@@ -570,12 +570,12 @@ function identify_layers (numberoflayers) {
 		var output_shape_string = "";
 		try {
 			if(i in model.layers) {
-				var shape = model.layers[i].getOutputAt(0).shape;
-				if(/((\[|,)\s*)\s*0\s*((\]|,)\s*)/.test(shape)) {
-					output_shape_string = "<span style='background-color: red'>Output:&nbsp;" + JSON.stringify(shape) + "</span>";
+				var shape = JSON.stringify(model.layers[i].getOutputAt(0).shape);
+				if(/((\[|,)\s*)\s*0\s*((\]|,)\s*)/.test(shape) || /\[\s*(0,?\s*?)+\s*\]/.test(shape)) {
+					output_shape_string = "<span style='background-color: red'>Output:&nbsp;" + shape + "</span>";
 					output_shape_string = output_shape_string.replace("null,", "");
 				} else {
-					output_shape_string = "Output:&nbsp;" + JSON.stringify(shape);
+					output_shape_string = "Output:&nbsp;" + shape;
 					output_shape_string = output_shape_string.replace("null,", "");
 				}
 			}
