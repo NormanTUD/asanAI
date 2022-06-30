f7774e6e69bd2fe27996c1b84f0c9dc88f165091
Norman Koch <kochnorman31@gmail.com>
2022-06-30 10:00:46 +0200

    speed improvements

--- index.php
+++ index.php
@@ -178,7 +178,7 @@ $GLOBALS['minify'] = 0;
 					<div id="logo_ribbon" class="ribbon_tab_content" title="Logo">
 						<div class="ribbon-group">
 							<div class="ribbon-toolbar" style="width:110px">
-								<img height=110 src="favico_tb.png" />
+								<img height=110 alt="asanAI Logo" src="favico_tb.png" />
 							</div>
 						</div>
 					</div>
