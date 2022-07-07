ec84e330933c0285660f05ce8397f72bdf0e8066
Norman Koch <kochnorman31@gmail.com>
2022-07-07 10:32:29 +0200

    added manifest

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
 		<title>asanAI</title>
 		<meta charset="utf-8">
-
+		<link rel="manifest" href="manifest.json" />
 
 		<script>
 			function hasWebGL() {
