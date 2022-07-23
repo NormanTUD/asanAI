dac5c6df36371a9757b756690e1439c09177e801
Norman Koch <norman.koch@tu-dresden.de>
2022-07-24 00:11:04 +0200

    removed unused div

--- index.php
+++ index.php
@@ -244,18 +244,16 @@ $GLOBALS['minify'] = 0;
 										<button id="reset_model" onclick="init_page_contents($('#dataset').val())">Reset Network</button>
 									</td>
 								</tr>
-								<div>
-									<tr>
-										<td>
-											Dataset:
-										</td>
-										<td colspan=2>
-											<select id="model_dataset" onchange="xy_data=null;change_model_dataset();" style="width: 204px">
-											</select>
-											<button id="load_weights_button" disabled="true" onclick="load_weights(1)" position="right" data-intro="Click here to load pretrained weights for the chosen model">Load</button>
-										</td>
-									</tr>
-								</div>
+								<tr>
+									<td>
+										Dataset:
+									</td>
+									<td colspan=2>
+										<select id="model_dataset" onchange="xy_data=null;change_model_dataset();" style="width: 204px">
+										</select>
+										<button id="load_weights_button" disabled="true" onclick="load_weights(1)" position="right" data-intro="Click here to load pretrained weights for the chosen model">Load</button>
+									</td>
+								</tr>
 								<tr>
 									<td>Shapes: </td>
 									<td><input type="text" value="" style="width: 105px;" onchange="update_input_shape()" readonly id="inputShape" />&nbsp;&rarr;&nbsp;<input type="text" value="" style="width: 102px;" readonly id="outputShape" /></td>
