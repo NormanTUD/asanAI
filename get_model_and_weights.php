fdd04e5e72d536515aa95e9a86c87ea08378a1c2
vm norman <norman.koch@tu-dresden.de>
2022-08-08 18:42:43 +0200

    fixed checks

--- get_model_and_weights.php
+++ get_model_and_weights.php
@@ -5,13 +5,9 @@
 		$model_id = $_GET["id"];
 		if(model_is_public($model_id) || can_edit_model(get_user_from_model_id($model_id), $model_id)) {
 			$user = get_user_id_from_session_id($_COOKIE["session_id"]);
-			if(is_null($user)) {
-				print "User doesn't exist.";
-			} else {
-				$user_is_admin = is_admin() ? 1 : 0;
-				$query = "select model_weights from model where id = ".esc($model_id)." and ((is_public = true and reviewed = true) or user_id = ".esc($user).") or $user_is_admin";
-				print get_single_value_from_query($query);
-			}
+			$user_is_admin = is_admin() ? 1 : 0;
+			$query = "select model_weights from model where id = ".esc($model_id)." and ((is_public = true and reviewed = true) or user_id = ".esc($user).") or $user_is_admin";
+			print get_single_value_from_query($query);
 		} else {
 			print "You don't have the permission.";
 		}
