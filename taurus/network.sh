ba9faa4f85ab43a1b8ffccd11ca10271e0ac5106
Norman Koch <kochnorman31@gmail.com>
2022-09-07 14:50:26 +0200

    use ml on taurus

--- taurus/network.sh
+++ taurus/network.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH --partition=alpha
 #SBATCH --mem=10000MB
@@ -6,16 +6,20 @@
 
 exec &> >(tee -a stdout.txt)
 
-asanaienv="$HOME/asanaienv"
+#asanaienv="$HOME/asanaienv"
+#cd
+#if [ -d $asanaienv ]; then
+#	echo "$asanaienv exists"
+#else
+#	python3 -m venv asanaienv
+#	pip install tensorflow tensorflowjs scikit-image keras
+#fi
+#source asanaienv/bin/activate
+#cd -
 
-cd
-if [ -d $asanaienv ]; then
-	echo "$asanaienv exists"
-else
-	python3 -m venv asanaienv
-	pip3 install tensorflow tensorflowjs scikit-image keras
-fi
-source asanaienv/bin/activate
-cd -
+ml modenv/hiera
+ml GCC/10.3.0
+ml OpenMPI/4.1.1
+ml TensorFlow/2.6.0-CUDA-11.3.1
 
 python3 network.py
