7e47a3bd61721555d2d31d94160dc1f16ff31b33
Norman Koch <kochnorman31@gmail.com>
2022-09-10 02:56:47 +0200

    better drawing way

--- explain.js
+++ explain.js
@@ -751,11 +751,11 @@ function draw_internal_states (layer, inputs, applied) {
 
 			if(canvas_output.length && canvas_input.length) {
 				for (var j = 0; j < canvas_input.length; j++) {
-					var img_input = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', input: ' + j + '" src="' + canvas_input[j].toDataURL() + '" />';
+					var img_input = canvas_input[j];
 					for (var i = 0; i < canvas_output.length; i++) {
-						var img_output = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', output: ' + j + '" src="' + canvas_output[i].toDataURL() + '" />';
+						var img_output = canvas_output[i];
 						if(Object.keys(canvas_kernel).includes(i + '')) {
-							var img_kernel = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', kernel: ' + i + '" src="' + canvas_kernel[i].toDataURL() + '" />';
+							var img_kernel = canvas_kernel[i];
 							if(layer == 0) {
 								input.append(img_input).show();
 							}
@@ -769,11 +769,11 @@ function draw_internal_states (layer, inputs, applied) {
 					}
 				}
 			} else if (canvas_output.length && canvas_input.nodeName == "CANVAS") {
-				var img_input = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', output" src="' + canvas_input.toDataURL() + '" />';
+				var img_input = canvas_input;
 				for (var i = 0; i < canvas_output.length; i++) {
-					var img_output = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', output: ' + i + '" src="' + canvas_output[i].toDataURL() + '" />';
+					var img_output = canvas_output[i];
 					if(Object.keys(canvas_kernel).includes(i + '')) {
-						var img_kernel = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', kernel: ' + i + '" src="' + canvas_kernel[i].toDataURL() + '" />';
+						var img_kernel = canvas_kernel[i];
 						kernel.append(img_kernel).show();
 						output.append(img_output).show();
 					} else {
@@ -784,9 +784,9 @@ function draw_internal_states (layer, inputs, applied) {
 				}
 			} else {
 				if(canvas_input.nodeName == "CANVAS") {
-					var img_input = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', output" src="' + canvas_output.toDataURL() + '" />';
+					var img_input = canvas_output;
 					if(canvas_output.nodeName == "CANVAS") {
-						var img_output = '<img class="layer_data_flow_item" alt="Layer ' + layer + ', input" src="' + canvas_output.toDataURL() + '" />';
+						var img_output = canvas_output;
 						input.append(img_input).show();
 						output.append(img_output).show();
 					} else {
