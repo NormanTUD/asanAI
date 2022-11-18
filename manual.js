12f08d50f6637697a1ddb24fd2c9d49b97a65b39
Norman Koch <kochnorman31@gmail.com>
2022-11-18 11:36:03 +0100

    handbook first draft

--- manual.js
+++ manual.js
@@ -39,7 +39,6 @@ function toc () {
 };
 
 async function get_network_type_result_by_array (layer_type, array, config, expand_dims=1) {
-	tf.engine().startScope();
 	var tensor = tf.tensor(array);
 	config["inputShape"] = tensor.shape;
 	var layer = null;
@@ -51,35 +50,56 @@ async function get_network_type_result_by_array (layer_type, array, config, expa
 
 	log("image tensor before applying:", tensor.arraySync());
 	var res = await layer.apply(tensor).arraySync();
-	log(res);
+	log("applied:", res);
 
-	tf.engine().endScope();
-
-	return res;
+	return [res, layer];
 }
 
 // await get_network_type_result_by_array("dense", [1, 2, 3], {units: 1, kernelInitializer: "ones" })
 
 // simulate_layer_on_image($("#tftestimg"), $("#tftestcanvas"), "conv2d", {filters: 1, kernelSize: [2, 2], kernelInitializer: "randomUniform", activation: "relu", strides: [1, 1] })
-async function simulate_layer_on_image(img_element, canvas, layer_type, config) {
+async function simulate_layer_on_image(img_element, internal_canvas_div, out_canvas_div, layer_type, config) {
+	tf.engine().startScope();
 	if(typeof(img_element) == "object") {
 		img_element = img_element[0];
-	}
-
-	if(typeof(canvas) == "object") {
-		canvas = canvas[0];
 	}
 
 	var img = tf.browser.fromPixels(img_element); 
 	img = img.div(255);
 
-	var result = await get_network_type_result_by_array(layer_type, img.arraySync(), config);
+	var [result, layer] = await get_network_type_result_by_array(layer_type, img.arraySync(), config, 1);
+	log("layer:", layer);
 
-	var tensor = tf.tensor(result[0]);
+	var tensor = tf.tensor(result);
 
-	draw_grid(canvas, 1, result[0], 1, 1, "", "");
-	//tf.browser.toPixels(tensor, canvas);
+	$(internal_canvas_div).html("");
+	$(out_canvas_div).html("");
 
+	var layer_kernel_tensor = layer.kernel.val;
+
+	const indices = tf.tensor1d([3, 0, 1, 2], 'int32');
+
+	layer_kernel_tensor = layer_kernel_tensor.transpose([3, 0, 1, 2]);
+
+	var layer_kernel = layer_kernel_tensor.arraySync();
+
+	log("kernel-shape:", layer_kernel_tensor.shape);
+	log("kernel:", layer_kernel);
+
+	for (var i = 0; i < layer.kernel.val.shape[0]; i++) {
+		var id = uuidv4()
+		$("<canvas class='kernel_images' id='" + id + "'></canvas>").appendTo(internal_canvas_div);
+		draw_grid($("#" + id)[0], 10, layer_kernel[i], 1, 1, "", "");
+	}
+
+	for (var i = 0; i < tensor.shape[0]; i++) {
+		var id = uuidv4()
+		$("<canvas class='out_images' id='" + id + "'></canvas>").appendTo(out_canvas_div);
+		draw_grid($("#" + id)[0], 1, result[i], 1, 1, "", "");
+		//tf.browser.toPixels(tensor, canvas);
+	}
+
+	tf.engine().endScope();
 	return result;
 }
 
