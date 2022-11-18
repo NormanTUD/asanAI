453cae2998c82301a08efc6712716760ece9c521
Norman Koch <kochnorman31@gmail.com>
2022-11-18 15:19:40 +0100

    handbook first draft

--- manual.js
+++ manual.js
@@ -49,17 +49,17 @@ async function get_network_type_result_by_array (layer_type, array, config, expa
 		var type = reg[i];
 		if(Object.keys(config).includes(type + "Regularizer")) {
 			if(config[type + "Regularizer"].hasL1 && config[type + "Regularizer"].hasL2) {
-				var cfg = {"className": tf.regularizers.l1l2, config: { "l1": config[type + "Regularizer"]["l1"], "l2": config[type + "Regularizer"]["l2"] }};
+				var cfg = {"className": "L1L2", config: { "l1": config[type + "Regularizer"]["l1"], "l2": config[type + "Regularizer"]["l2"], hasL1: true, hasL2: true }};
 				config[type + "Regularizer"] = cfg;
 				//config[type + "Regularizer"] = tf.regularizers.l1l2(cfg);
 
 			} else if(config[type + "Regularizer"].hasL1 && !config[type + "Regularizer"].hasL2) {
-				var cfg = {"className": tf.regularizers.l1, config: { "l1": config[type + "Regularizer"]["l1"] }};
+				var cfg = {"className": "L1", config: { "l1": config[type + "Regularizer"]["l1"], hasL1: true, hasL2: false }};
 				config[type + "Regularizer"] = cfg;
 				//config[type + "Regularizer"] = tf.regularizers.l1(cfg);
 
 			} else if(!config[type + "Regularizer"].hasL1 && config[type + "Regularizer"].hasL2) {
-				var cfg = {"className": tf.regularizers.l2l2, config: { "l1": 0.01, "l2": config[type + "Regularizer"]["l2"] }};
+				var cfg = {"className": "L1L2", config: { "l1": 0.01, "l2": config[type + "Regularizer"]["l2"], hasL1: false, hasL2: true }};
 				config[type + "Regularizer"] = cfg;
 				//config[type + "Regularizer"] = tf.regularizers.l2(cfg);
 	
