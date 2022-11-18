e7063b94a5a18a19f966c5c0b0e5b5aaa87dedea
Norman Koch <kochnorman31@gmail.com>
2022-11-18 10:25:15 +0100

    handbook first draft

--- manual.js
+++ manual.js
@@ -39,9 +39,6 @@ function toc () {
 };
 
 async function get_network_type_result_by_array (layer_type, array, config, expand_dims=1) {
-	log("layer_type:", layer_type);
-	log("config:", config);
-
 	tf.engine().startScope();
 	var tensor = tf.tensor(array);
 	config["inputShape"] = tensor.shape;
@@ -52,9 +49,9 @@ async function get_network_type_result_by_array (layer_type, array, config, expa
 		tensor = tensor.expandDims();
 	}
 
-	log("tensor-shape:", tensor.shape);
-
+	log("image tensor before applying:", tensor.arraySync());
 	var res = await layer.apply(tensor).arraySync();
+	log(res);
 
 	tf.engine().endScope();
 
@@ -64,8 +61,6 @@ async function get_network_type_result_by_array (layer_type, array, config, expa
 // await get_network_type_result_by_array("dense", [1, 2, 3], {units: 1, kernelInitializer: "ones" })
 
 async function simulate_layer_on_image(img_element, canvas, layer_type, config) {
-	log("!!! config", config);
-
 	if(typeof(img_element) == "object") {
 		img_element = img_element[0];
 	}
@@ -74,18 +69,14 @@ async function simulate_layer_on_image(img_element, canvas, layer_type, config) 
 		canvas = canvas[0];
 	}
 
-	log("img_element:", img_element);
-	log("canvas:", canvas);
-
 	var img = tf.browser.fromPixels(img_element); 
+	img = img.div(255);
 
 	var result = await get_network_type_result_by_array(layer_type, img.arraySync(), config);
 
 	var tensor = tf.tensor(result[0]);
 
-	log(tensor);
-
-	draw_grid(canvas, 1, result[0], 1, 0, "", "");
+	draw_grid(canvas, 1, result[0], 1, 1, "", "");
 	//tf.browser.toPixels(tensor, canvas);
 
 	return result;
