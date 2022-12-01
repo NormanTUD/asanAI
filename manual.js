f350db663a5f7eeecc32cd83572ae1dd554a270f
Norman Koch <norman.koch@tu-dresden.de>
2022-12-01 19:21:16 +0100

    removed old code

--- manual.js
+++ manual.js
@@ -541,7 +541,7 @@ async function start_test_training() {
 	current_model.add(tf.layers.dense({units: 2, activation: "linear"}));
 	current_model.add(tf.layers.dense({units: 1, activation: "linear"}));
 
-	await train_example(current_model, 200, t_x, t_y, "meanSquaredError", 10, "adam");
+	await train_example(current_model, 250, t_x, t_y, "meanSquaredError", 10, "adam");
 	tf.engine().endScope();
 }
 
