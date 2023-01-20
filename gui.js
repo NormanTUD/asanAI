27944bff1d44721f46cdd1c3935d7dee3231ca22
Norman Koch <kochnorman31@gmail.com>
2023-01-20 10:16:46 +0100

    ave image again

--- gui.js
+++ gui.js
@@ -3310,8 +3310,8 @@ function add_new_category() {
 			'<button style="' + webcam_button_style + '" class="webcam_data_button" onclick="take_image_from_webcam(this)">&#128248; Take image from webcam</button>' +
 			'<button style="' + webcam_button_style + '" class="webcam_data_button webcam_series_button" onclick="take_image_from_webcam_n_times(this)">&#128248; Take 10 images from webcam (1s apart)</button>' +
 			'<button class="delete_category_button" onclick="delete_category(this)">Delete this category</button></div>' +
-			'<div id="' + uuid + '"></div>' //+
-			//`<button onclick="add_image_to_category($('#${uuid}_sketcher')[0].toDataURL(), ${label_nr});event.preventDefault();atrament_data['${uuid}_sketcher']['atrament'].clear();">Save this image</button>`
+			'<div id="' + uuid + '"></div>' +
+			`<button onclick="add_image_to_category($('#${uuid}_sketcher')[0].toDataURL(), ${label_nr});event.preventDefault();atrament_data['${uuid}_sketcher']['atrament'].clear();">Save this image</button>`
 		).appendTo("#own_images_container");
 		$('<form method="post" enctype="multipart/form-data"><input onkeyup="rename_labels(1)" class="own_image_label" value="label ' + label_nr + '" /><input type="file" class="own_image_files" multiple accept="image/*"><br/></form>').appendTo($(".own_image_upload_container")[n]);
 		$('<div class="own_images"></div>').appendTo($(".own_image_upload_container")[n]);
