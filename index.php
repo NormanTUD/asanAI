5ba1d4bb8e430469c0eb870a6be1b78961af3614
Norman Koch <norman.koch@tu-dresden.de>
2023-03-16 14:23:14 +0100

    emoji instead of text

--- index.php
+++ index.php
@@ -1088,7 +1088,7 @@
 
 
 				<div id="help" style="display: none"></div>
-				<div id="toggle_layers_button"><button style="width: 100%" onclick="toggle_layers()">Show layers</button></div>
+				<div id="toggle_layers_button" class="show_cosmo_wave_2"><button style="width: 100%" onclick="toggle_layers()"><span class="large_button">&#9881;&#129302;</span></button></div>
 
 				<div class="side_by_side_container">
 					<div id="layers_container_left" class="left_side">
