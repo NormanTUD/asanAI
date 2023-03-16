e152446dc3a9b7c40f1c540719ab57fd9b14d26c
Norman Koch <norman.koch@tu-dresden.de>
2023-03-16 10:56:07 +0100

    nicer version

--- gui.js
+++ gui.js
@@ -4961,7 +4961,7 @@ function invert_elements_in_dark_mode () {
 }
 
 function green_marker (element) {
-	$(element).parent().find(".atrament_buttons").removeClass("green_icon");
+	$(element).parent().parent().find(".green_icon").removeClass("green_icon")
 	$(element).addClass("green_icon");
 
 }
@@ -4971,9 +4971,9 @@ function get_drawing_board_on_page (indiv, idname, customfunc) {
 		customfunc = "";
 	}
 	var code = `<form>
-		<a class='atrament_buttons invert_in_dark_mode green_icon' onclick="atrament_data['${idname}']['atrament'].mode = 'brush'; $(this).parent().find('.pen_size_slider').show(); $(this).parent().find('.jscolor').show(); green_marker(this);"><img width=32 src='pen.png'/></a>
-		<a class='atrament_buttons invert_in_dark_mode' onclick="atrament_data['${idname}']['atrament'].mode = 'fill'; $(this).parent().find('.pen_size_slider').hide(); $(this).parent().find('.jscolor').show(); green_marker(this); "><img width=32 src='Fill-icon.svg'></a>
-		<a class='atrament_buttons invert_in_dark_mode' onclick="atrament_data['${idname}']['atrament'].mode = 'erase'; $(this).parent().find('.pen_size_slider').show(); $(this).parent().find('.jscolor').hide(); green_marker(this);"><img width=32 src='Eraser_icon.svg'/></a>
+		<span class='invert_in_dark_mode'><a class='atrament_buttons green_icon' onclick="atrament_data['${idname}']['atrament'].mode = 'brush'; $(this).parent().find('.pen_size_slider').show(); $(this).parent().find('.jscolor').show(); green_marker(this);"><img width=32 src='pen.png'/></a></span>
+		<span class='invert_in_dark_mode'><a class='atrament_buttons' onclick="atrament_data['${idname}']['atrament'].mode = 'fill'; $(this).parent().find('.pen_size_slider').hide(); $(this).parent().find('.jscolor').show(); green_marker(this); "><img width=32 src='Fill-icon.svg'></a></span>
+		<span class='invert_in_dark_mode'><a class='atrament_buttons' onclick="atrament_data['${idname}']['atrament'].mode = 'erase'; $(this).parent().find('.pen_size_slider').show(); $(this).parent().find('.jscolor').hide(); green_marker(this);"><img width=32 src='Eraser_icon.svg'/></a></span>
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<span onclick="clear_attrament('${idname}');${customfunc}" class='atrament_buttons'>&#10060;</span><br>
 		<input type="text" name="value" id='${idname}_colorpicker' class="show_data jscolor" style='width: 50px' value="#000000" onchange="atrament_data['${idname}']['atrament'].color='#'+this.value;" />
