d6a4d894631afa0d7ba9b48aefa0fbf04d41edec
Norman Koch <norman.koch@tu-dresden.de>
2023-03-17 09:32:05 +0100

    fix

--- traindata/augment.sh
+++ traindata/augment.sh
@@ -13,5 +13,5 @@ gmic $INPUT +sketchbw 1 reverse "blur[-1]" 3 "blend[-2,-1]" overlay -o "${basena
 gmic $INPUT sponge , -o "${basename}_sponge.png"
 gmic $INPUT +drawing , -o "${basename}_drawing.png"
 gmic $INPUT cartoon 3,50,10,0.25,3,16 -o "${basename}_cartoon.png"
-gmic $INPUT +hardsketchbw 200,70,0.1,10 median[-1] 2 +local reverse blur[-1] 3 blend[-2,-1] overlay done -o "${basename}_hardsketchbw .png"
+#gmic $INPUT +hardsketchbw 200,70,0.1,10 median[-1] 2 +local reverse blur[-1] 3 blend[-2,-1] overlay done -o "${basename}_hardsketchbw .png"
 gmic $INPUT +deform[0] 10 +deform[0] 20 -o "${basename}_deform.png"
