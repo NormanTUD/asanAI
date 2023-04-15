2f1e0472bc5c267ca733098dc85315a145efbcdb
Norman Koch <norman.koch@tu-dresden.de>
2023-04-15 15:52:36 +0200

    more awaits

--- explain.js
+++ explain.js
@@ -1048,7 +1048,7 @@ async function predict_maximally_activated (item, force_category) {
 	}
 	$(item).after("<pre class='maximally_activated_predictions'>" + results + "</pre>");
 
-	predict($('#predict_own_data').val())
+	await predict($('#predict_own_data').val())
 }
 
 async function draw_maximally_activated_neuron (layer, neuron) {
