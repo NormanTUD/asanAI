233298f2c0b1bb00ae70312f3ec5fcd893316cc9
Norman Koch <norman.koch@tu-dresden.de>
2023-04-18 13:55:47 +0200

    geht halbwegs

--- gui.js
+++ gui.js
@@ -5536,28 +5536,8 @@ function chose_next_manicule_target () {
 
 					if(current_skill_nr_matches_required_skill_number) {
 						log("current_skill_nr_matches_required_skill_number");
-
-
-						if(!current_skill_nr_matches_required_skill_number) {
-							possible = false;
-						}
-
-						/*
-						if(x.nodeName.startsWith("BUTTON")) {
-							log(`
-							var current_key = ${current_key} = Object.keys(req_full)[n];
-
-							var full_req_part_is_part_of_current_skills = ${full_req_part_is_part_of_current_skills} = Object.keys(current_skills).includes(current_key)
-							var current_skill_nr_matches_required_skill_number = ${current_skill_nr_matches_required_skill_number} = ${current_skills[current_key]} == ${req_full[current_key]};
-							`);
-
-							//log(">>>>>>", "req_full", req_full, "element:", x, "full_req_part_is_part_of_current_skills:", full_req_part_is_part_of_current_skills, "current_skill_nr_matches_required_skill_number:", current_skill_nr_matches_required_skill_number, "possible?", possible, "<<<<<<");
-						}
-						*/
-						/*
 					} else {
-						log("NOT current_skill_nr_matches_required_skill_number");
-						*/
+						possible = false;
 					}
 				}
 			}
@@ -5571,8 +5551,6 @@ function chose_next_manicule_target () {
 
 			if(possible) {
 				//log("==== Element: ", x, "req_full", req_full);
-				log("!!!!!!!!!!!!!!!!!!!!!!!!! SHOW THIS NOW:", $(x));
-				$(x).show();
 				//log("It seems that current_skills allows you to display index " + i, x)
 				possible_indices.push({"index": i, "length": req_full.length});
 			}
