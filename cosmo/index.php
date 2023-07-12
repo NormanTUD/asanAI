f8bfe70d45765b8110733c458897ddef954e1236
Norman Koch <norman.koch@tu-dresden.de>
2023-07-12 09:40:40 +0200

    fix?

--- cosmo/index.php
+++ cosmo/index.php
@@ -1,15 +1,12 @@
 <?php
-// Get the current script's directory
-$currentDirectory = dirname($_SERVER['SCRIPT_NAME']);
-
 // Get the parent directory's URL
-$parentDirectory = rtrim($currentDirectory, '/');
-$parentDirectory = preg_replace('/cosmo\/?$/', '', $parentDirectory);
+$parentDirectory = dirname($_SERVER['SCRIPT_NAME']);
 
 // Append the parent directory's URL with the index.php file and query parameters
-$redirectURL = $parentDirectory . '/index.php?' . $_SERVER['QUERY_STRING'] . '&start_cosmo=1';
+$redirectURL = rtrim($parentDirectory, '/') . '/../index.php?' . $_SERVER['QUERY_STRING'] . '&start_cosmo=1';
 
 // Perform the redirect
 header("Location: $redirectURL");
 exit;
 ?>
+
