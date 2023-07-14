39a70e2af4d26e74ea26b39f89807c203ed6c725
Norman Koch <norman.koch@tu-dresden.de>
2023-07-14 10:18:53 +0200

    fix

--- present.js
+++ present.js
@@ -18,7 +18,6 @@ function showFullScreen(divs, currentDivPresentationIndex) {
 
 	$(div).show();
 
-	log("showFullScreen", div);
 	div.style.width = '100%';
 	div.style.height = '100%';
 	div.style.position = 'fixed';
