09ea1c0a2452352a04df5216969a168230f2e1bc
Norman Koch <norman.koch@tu-dresden.de>
2023-08-07 09:27:59 +0200

    fix

--- debug.js
+++ debug.js
@@ -11,7 +11,7 @@ function memory_leak_debugger (name="", oldNumTensors=null) {
 	if(name && oldNumTensors) {
 		if(numTensors > oldNumTensors) {
 			if(name != "") {
-				console.info("MEMORY LEAK WHILE (but not neccessarily in) " + name + "! +" + Math.abs(oldNumTensors - numTensors) + " Tensors (currently at " + numTensors + " tensors)");
+				console.info("MEMORY LEAK WHILE (but not neccessarily in) " + name + "! +" + Math.abs(oldNumTensors - numTensors) + " Tensors (" + numTensors + " in total)");
 			} else {
 				console.info("MEMORY LEAK! New tensors: " + Math.abs(oldNumTensors - numTensors));
 			}
