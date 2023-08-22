47ee302516199a2c27d541f5e4d6ac5d143f465e
Norman Koch <norman.koch@tu-dresden.de>
2023-08-22 10:26:12 +0200

    fix

--- index.php
+++ index.php
@@ -1651,10 +1651,10 @@ text-align: end;
 			<span id="memory_debugger_div"></span>
 		</div>
 
-		<div style="display: flex; justify-content: center; align-items: center; height: 100vh; pointer-events: none; background-color: white; user-select: none;" id="loading_icon_wrapper">
-			<img src="gui/scads_logo.svg" alt="Loading..." style="position: absolute; left: 10px; top: 10px; height: 67px">
-			<img src="gui/logo.svg" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 67px">
-			<img src="gui/loading_icon.gif" alt="Loading..." style="max-width: 100%;">
+		<div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw; pointer-events: none; background-color: white; user-select: none;" id="loading_icon_wrapper">
+			<img src="gui/scads_logo.svg" alt="Loading..." style="position: absolute; left: 10px; top: 10px; height: 8vw">
+			<img src="gui/logo.svg" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
+			<img src="gui/loading_icon.gif" alt="Loading..." style="max-width: 20vw;">
 			<br>
 			<div id="load_msg"></div>
 		</div>
