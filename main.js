81290c50482d89203e57a1100597cb2a08f69813
Norman Koch <norman.koch@tu-dresden.de>
2023-08-22 07:46:10 +0200

    less frequent write_model_summary

--- main.js
+++ main.js
@@ -457,7 +457,7 @@ $(document).ready(async function() { var start_tensors = memory_leak_debugger();
 	setInterval(fix_viz_width, 700);
 	setInterval(check_number_values, 100);
 	setInterval(display_delete_button, 300);
-	setInterval(write_model_summary, 300);
+	setInterval(write_model_summary_wait, 1000);
 	//setInterval(write_descriptions, 1000);
 	setInterval(disable_everything_in_last_layer_enable_everyone_else_in_beginner_mode, 400);
 
