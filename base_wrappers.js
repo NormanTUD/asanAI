7c83c30eaf352e0bed1f7b2775794c429210f4b4
Norman Koch <norman.koch@tu-dresden.de>
2023-08-27 12:50:15 +0200

    base_wrappers

---
+++ base_wrappers.js
@@ -0,0 +1,15 @@
+function tensor1d (...args) {
+	return tf.tensor1d(...args);
+}
+
+function tensor2d (...args) {
+	return tf.tensor2d(...args);
+}
+
+function tensor (...args) {
+	return tf.tensor(...args);
+}
+
+function oneHot (...args) {
+	return tf.oneHot(...args);
+}
