d8f00431d7d8c2e27217408b12b35b6504744a97
Norman Koch <norman.koch@tu-dresden.de>
2023-08-27 22:40:25 +0200

    fixed layer bug

--- gui.js
+++ gui.js
@@ -2747,7 +2747,6 @@ async function init_dataset_category() {
 
 async function clean_gui() {
 	reset_summary();
-	await write_error("");
 	await write_descriptions();
 }
 
@@ -6084,6 +6083,7 @@ function allow_editable_labels () {
 }
 
 function enable_every_layer () {
+	console.trace();
 	$(".configtable").find("input,select,button").prop("disabled", false);
 	$(".layer_setting").find("button").prop("disabled", false);
 }
@@ -6122,7 +6122,7 @@ function disable_flatten_layer () {
 
 function disable_everything_in_last_layer_enable_everyone_else_in_beginner_mode () {
 	try {
-		enable_every_layer();
+		//enable_every_layer();
 
 		if(mode == "beginner") {
 			$($(".configtable")[$(".configtable").length - 1]).find("input,select,button").prop("disabled", true);
