c4ec9419c969dc049ec5133efb5016fa9d119dd8
Norman Koch <norman.koch@tu-dresden.de>
2023-08-28 14:13:29 +0200

    remove old js minify

--- functions.php
+++ functions.php
@@ -342,27 +342,16 @@
 	}
 
 	function minify_js ($file, $async=0, $defer=0) {
-		if(0 && ($GLOBALS["minify"] || $file == "style.css") && $file != "ribbon.css" && $file != 'visualizations/d3.v5.min.js' && $file != "visualizations/three.min.js") {
-			if($async && $defer) {
-				print "<script async defer src='minify.php?file=$file'></script>";
-			} else if($async && !$defer) {
-				print "<script async src='minify.php?file=$file'></script>";
-			} else if(!$async && $defer) {
-				print "<script defer src='minify.php?file=$file'></script>";
-			} else {
-				print "<script src='minify.php?file=$file'></script>";
-			}
+		if($async && $defer) {
+			print "<script async defer src='$file'></script>";
+		} else if($async && !$defer) {
+			print "<script async src='$file'></script>";
+		} else if(!$async && $defer) {
+			print "<script defer defer src='$file'></script>";
 		} else {
-			if($async && $defer) {
-				print "<script async defer src='$file'></script>";
-			} else if($async && !$defer) {
-				print "<script async src='$file'></script>";
-			} else if(!$async && $defer) {
-				print "<script defer defer src='$file'></script>";
-			} else {
-				print "<script src='$file'></script>";
-			}
+			print "<script src='$file'></script>";
 		}
+
 		print "\n";
 	}
 
