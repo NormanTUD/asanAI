ad04b7cd30f1b1b6cbb2209af2b16f19902b10ef
Norman Koch <norman.koch@tu-dresden.de>
2023-08-31 09:12:13 +0200

    cloneCanvas

--- gui.js
+++ gui.js
@@ -6529,3 +6529,19 @@ function showOverlayWithText(text, title="") {
 		console.warn('Failed to display overlay.');
 	}
 }
+
+function cloneCanvas(oldCanvas) {
+	//create a new canvas
+	var newCanvas = document.createElement('canvas');
+	var context = newCanvas.getContext('2d');
+
+	//set dimensions
+	newCanvas.width = oldCanvas.width;
+	newCanvas.height = oldCanvas.height;
+
+	//apply the old canvas to the new one
+	context.drawImage(oldCanvas, 0, 0);
+
+	//return the new canvas
+	return newCanvas;
+}
