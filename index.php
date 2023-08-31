dade281353f8de109f513b910c7791b8df2d6b78
Norman Koch <norman.koch@tu-dresden.de>
2023-08-31 07:47:05 +0200

    fixed width of training graph

--- index.php
+++ index.php
@@ -1391,7 +1391,7 @@ text-align: end;
 									<br>
 									<div id="training_content">
 										<div id="simplest_training_data_visualization" style="display: none"></div>
-										<div style="display: none">
+										<div id="plotly_epoch_history_div" style="display: none">
 											<div id="plotly_epoch_history"></div>
 										</div>
 
