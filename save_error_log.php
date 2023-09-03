fc27f50ed5291ad855d8e498c694d5145ac8fee5
Norman Koch <norman.koch@tu-dresden.de>
2023-09-03 11:51:11 +0200

    check proper

--- save_error_log.php
+++ save_error_log.php
@@ -1,4 +1,33 @@
 <?php
+	include_once('functions.php');
+
+	function checkElementsInAllowedTypes($string) {
+		// Define the allowed element types
+		$allowed_types = array('table', 'tr', 'td', 'th', 'span', 'pre', 'tbody');
+
+		// Use a regular expression to find all opening and closing tags
+		preg_match_all('/<\/?(.*?)>/', $string, $matches);
+
+		if (empty($matches[1])) {
+			// No opening or closing tags found, return true
+			return true;
+		}
+
+		// Iterate through the matched elements
+		foreach ($matches[1] as $element) {
+			// Convert the element type to lowercase for case-insensitive comparison
+			$element = strtolower($element);
+
+			// Check if the element type is not in the allowed types
+			if (!in_array($element, $allowed_types)) {
+				return false;
+			}
+		}
+
+		// All matched elements are in the allowed types, return true
+		return true;
+	}
+
 	function receiveAndCheckHTML($log_file_dir, $html_code) {
 		// Create an array to store error messages
 		$error_messages = array();
@@ -11,19 +40,14 @@
 				// Generate a unique filename for the HTML file
 				$filename = uniqid() . '.html';
 
-				// Define the allowed HTML tags and attributes
-				$allowed_tags = '<table><tr><td><th><span><pre>';
-
-				// Remove any disallowed tags and attributes
-				$filtered_html = strip_tags($html_code, $allowed_tags);
-
 				// Check if the filtered HTML is empty (no allowed tags)
-				if (empty($filtered_html)) {
+				if (!checkElementsInAllowedTypes($html_code)) {
 					$error_messages[] = "The HTML code does not contain any allowed tags (only <table> and <span>).";
 				} else {
 					// Save the filtered HTML to the log file directory
 					$file_path = $log_file_dir . '/' . $filename;
-					file_put_contents($file_path, $filtered_html);
+					print(">>$html_code<<\n");
+					file_put_contents($file_path, $html_code);
 
 					if (file_exists($file_path)) {
 						$success_message = "HTML code saved successfully.";
@@ -46,9 +70,13 @@
 	// Example usage:
 	if (isset($_SERVER["REQUEST_METHOD"])) {
 		if ($_SERVER["REQUEST_METHOD"] === "POST") {
-			$html_code = $_POST["html_code"];
-			$log_file_dir = "debuglogs";
-			receiveAndCheckHTML($log_file_dir, $html_code);
+			$html_code = file_get_contents("php://input");
+			if ($html_code !== false) {
+				$log_file_dir = "debuglogs";
+				receiveAndCheckHTML($log_file_dir, $html_code);
+			} else {
+				echo json_encode(array("error" => "post html_code was not set"));
+			}
 		} else {
 			echo json_encode(array("error" => "was not requested with post"));
 		}
