933309bd89acfed5e50f40351594f5b8be587486
Norman Koch <norman.koch@tu-dresden.de>
2023-09-08 09:03:34 +0200

    set stage numbers

--- variables.js
+++ variables.js
@@ -1440,11 +1440,11 @@ async function _cosmo_set_environment (_ep, _max_number_of_files_per_category, _
 
 async function cosmo_stage_one () {
 	log("Cosmo stage 1");
-	var _ep  = parse_int(get_get("epochs", 20));
-	var _max_number_of_files_per_category = parse_int(get_get("max_number_of_files_per_category", 20));
+	var _ep  = parse_int(get_get("epochs", 5));
+	var _max_number_of_files_per_category = parse_int(get_get("max_number_of_files_per_category", 10));
 	var _vs = 0;
 
-	await _cosmo_set_environment(_ep, _max_number_of_files_per_category, _vs, 2);
+	await _cosmo_set_environment(_ep, _max_number_of_files_per_category, _vs, 10);
 
 	current_cosmo_stage = 1;
 }
@@ -1460,10 +1460,10 @@ async function cosmo_stage_two () {
 	set_retrain_button();
 
 	var _ep  = parse_int(get_get("epochs_stage_2", get_get("epochs", 20)));
-	var _max_number_of_files_per_category = parse_int(get_get("max_number_of_files_per_category_stage_2", get_get("max_number_of_files_per_category", 40)));
+	var _max_number_of_files_per_category = parse_int(get_get("max_number_of_files_per_category_stage_2", get_get("max_number_of_files_per_category", 50)));
 	var _vs = 15;
 
-	await _cosmo_set_environment(_ep, _max_number_of_files_per_category, _vs, 10);
+	await _cosmo_set_environment(_ep, _max_number_of_files_per_category, _vs, 20);
 	current_cosmo_stage = 2;
 }
 
