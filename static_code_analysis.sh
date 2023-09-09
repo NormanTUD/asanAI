4bb07a62b53806d25d447d538de9c85b76ae264e
Norman Koch <norman.koch@tu-dresden.de>
2023-09-09 21:36:32 +0200

    added stack trace

--- static_code_analysis.sh
+++ static_code_analysis.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+function get_parent_pid {
+	parent_pid=$(ps -o ppid= $1 | sed -e 's/\s//g')
+	parent_name=$(ps -p $parent_pid -o command | tr -d '\n' | sed -e 's/^COMMAND//' | sed -e "s/^/$parent_pid: /")
+	echo $parent_name >&2
+	echo $parent_pid
+}
+
+function process_callstack {
+	pid=$$
+	while [[ $pid -ne "1" ]]; do
+		pid=$(get_parent_pid $pid)
+	done
+}
+
 set -e
+
+function calltracer () {
+	echo 'Last file/last line:'
+	caller
+
+	process_callstack
+}
+trap 'calltracer' ERR
+
+ERROR=0
 
 NON_AWAITED_ASYNC=$(for i in $(grep "^async\s*function " *.js *.php | sed -e "s#.*async\s*function\s*##" | sed -e "s#\s*(.*##"); do
 	egrep "\s$i\s*\(" *.js *.php;
@@ -10,6 +34,7 @@ if [[ ! -z $NON_AWAITED_ASYNC ]]; then
 	echo "Command for finding non awaited JS functions:"
 
 	echo "$NON_AWAITED_ASYNC"
+	ERROR=1
 fi
 
 DOUBLE_DEFINED_FUNCS=$(ack "^\s*(async)?\s*function\s*" *.js | sed -e "s#.*\s*function\s*##" | sed -e "s#\s*(.*##" | sort | uniq -c | sort -nr | tac | egrep -v "^\s*[0-9]+\s*$" | egrep -v "^\s*1\s+.*$")
@@ -17,6 +42,7 @@ DOUBLE_DEFINED_FUNCS=$(ack "^\s*(async)?\s*function\s*" *.js | sed -e "s#.*\s*fu
 if [[ ! -z $DOUBLE_DEFINED_FUNCS ]]; then
 	echo "find double defined functions"
 	echo $DOUBLE_DEFINED_FUNCS
+	ERROR=1
 fi
 
 UNCALLED_FUNCS=$(for func_name in $(ack "^\s*(async)?\s*function\s*" *.js | sed -e "s#.*\s*function\s*##" | sed -e "s#\s*(.*##" | sort | grep -v _option); do
@@ -26,11 +52,10 @@ UNCALLED_FUNCS=$(for func_name in $(ack "^\s*(async)?\s*function\s*" *.js | sed 
 	fi
 done)
 
-echo $UNCALLED_FUNCS
-
 if [[ ! -z $UNCALLED_FUNCS ]]; then
 	echo "find functions that are defined, yet never called:"
 	echo "$UNCALLED_FUNCS"
+	ERROR=1
 fi
 
 #echo "Find untested functions, listed by number of occurences"
@@ -38,3 +63,9 @@ fi
 #	grep $i tests.js 2>&1 >/dev/null || NUM_OCC=$(ack "(?:(?:^\s*)|=|\"|\()\s*(?:await)?\s*$i\s*\(" *.js | grep -v function | wc -l);
 #	echo "$NUM_OCC: $i is untested currently"; 
 #done | sort -nr | tac
+
+if [[ "$ERROR" == "1" ]]; then
+	exit 4
+else
+	exit 0
+fi
