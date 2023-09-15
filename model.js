52811b3310ef179fe08ad4c44fb92eeaa70ec5f8
Norman Koch <norman.koch@tu-dresden.de>
2023-09-15 11:43:18 +0200

    less error msgs

--- model.js
+++ model.js
@@ -80,7 +80,7 @@ async function _create_model () {
 				wrn("Currently, the model is undefined. This may be fatal, but may also not be");
 			} else if(("" + e).includes("model.layers[i] is undefined")) {
 				wrn("" + e);
-			} else if(("" + e).includes("Inputs to DepthwiseConv2D should have rank")) {
+			} else if(("" + e).includes("Inputs to DepthwiseConv2D should have rank") || ("" + e).includes("Inputs to SeparableConv2D should have rank")) {
 				wrn("" + e);
 			} else {
 				await except("ERROR1", "" + e);
