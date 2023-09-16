3197fb7064bbbb50a3d4332a1740359af6ff806d
Norman Koch <norman.koch@tu-dresden.de>
2023-09-16 13:13:26 +0200

    added comment

--- gui.js
+++ gui.js
@@ -6127,7 +6127,7 @@ function label_debugger_icon_ok () {
 	} else {
 		if(model.layers.last().output.shape.length == 2 && model.layers.last().name.startsWith("dense")) {
 			$("#label_debugger_icon").html("<span style='background-color: orange; color: black;'>[No labels]</span>").show();
-			get_label_data();
+			get_label_data(); // await not possible here
 		} else {
 			$("#label_debugger_icon").html("").hide();
 		}
