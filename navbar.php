fd9a035965b98e6722d206ac1ba9ccf463f7bec8
Norman Koch <norman.koch@tu-dresden.de>
2023-09-24 13:29:32 +0200

    fixed tabs

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <div id="navbar1" class="user_select_none" style="display: flex">
 	<ul class="navi_list">
 		<li><a id="training_data_tab_label" href="#training_data_tab"><span class='TRANSLATEME_data'></span></a></li>
-		<li><a href="#visualization_tab" id="visualization_tab_label"><span class='TRANSLATEME_model_visualization'></span></a></li>
-		<li><a href="#summary_tab" onclick="write_model_summary_wait()"><span class="TRANSLATEME_summary"></span></a></li>
+		<li><a id="visualization_tab_label" href="#visualization_tab" ><span class='TRANSLATEME_model_visualization'></span></a></li>
+		<li><a id="summary_tab_label" href="#summary_tab" onclick="write_model_summary_wait()"><span class="TRANSLATEME_summary"></span></a></li>
 		<li><a id="own_image_data_label" href="#own_image_data"><span class="TRANSLATEME_own_images"></span></a></li>
 		<li><a id="own_tensor_data_label" href="#own_tensor_data"><span class="TRANSLATEME_own_tensors"></span></a></li>
 		<li><a id="own_csv_data_label" href="#own_csv_data"><span class="TRANSLATEME_own_csv"></span></a></li>
