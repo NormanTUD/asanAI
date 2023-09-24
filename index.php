0d58feef6a7dbcf133e43878ed058b661f88b7dc
Norman Koch <norman.koch@tu-dresden.de>
2023-09-24 12:27:30 +0200

    moved to load_msg

--- index.php
+++ index.php
@@ -857,15 +857,8 @@
 			</span>
 			<span id="memory_debugger_div"></span>
 		</div>
-
-		<div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw; pointer-events: none; background-color: white; user-select: none;" id="loading_icon_wrapper">
-			<img src="_gui/scads_logo.svg" alt="Loading..." style="position: absolute; left: 10px; top: 10px; height: 8vw">
-			<img src="_gui/logo.svg" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
-			<img src="_gui/loading_icon.gif" alt="Loading..." style="max-width: 20vw;">
-			<br>
-			<div id="load_msg"></div>
-		</div>
 <?php
+		include("load_msg.php");
 		_js("libs/prism/prism.js");
 		_js("libs/prism/prism-python.min.js");
 		_js("libs/jscolor.js", 1, 1);
