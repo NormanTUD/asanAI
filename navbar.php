ce9838d4bc69a318e1e792ffd2e5cc67265bd301
Norman Koch <norman.koch@tu-dresden.de>
2023-09-24 13:03:13 +0200

    moved stuff to seperate files

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<div id="navbar1" class="user_select_none" style="display: flex">
+	<ul class="navi_list">
+		<li><a id="training_data_tab_label" href="#training_data_tab"><span class='TRANSLATEME_data'></span></a></li>
+		<li><a href="#visualization_tab" id="visualization_tab_label"><span class='TRANSLATEME_model_visualization'></span></a></li>
+		<li><a href="#summary_tab" onclick="write_model_summary_wait()"><span class="TRANSLATEME_summary"></span></a></li>
+		<li><a id="own_image_data_label" href="#own_image_data"><span class="TRANSLATEME_own_images"></span></a></li>
+		<li><a id="own_tensor_data_label" href="#own_tensor_data"><span class="TRANSLATEME_own_tensors"></span></a></li>
+		<li><a id="own_csv_data_label" href="#own_csv_data"><span class="TRANSLATEME_own_csv"></span></a></li>
+		<li><a id="tfvis_tab_label" href="#tfvis_tab"><span class="TRANSLATEME_training"></span></a></li>
+		<li><a id="predict_tab_label" href="#predict_tab"><span class="TRANSLATEME_predict"></span></a></li>
+		<li><a id="code_tab_label" href="#code_tab"><span class="TRANSLATEME_code"></span></a></li>
+	</ul>
+	<span id="toggle_layer_view_button" style="" onclick="toggle_layer_view()">&#128470;</span>
+</div>
+<hr id="hr_nav">
