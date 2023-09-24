533bccb7325d0063bdd5a4164e4c86181fa01f86
Norman Koch <norman.koch@tu-dresden.de>
2023-09-24 13:06:59 +0200

    moved stuff to divs

--- index.php
+++ index.php
@@ -250,28 +250,10 @@
 ?>
 
 			<div id="maindiv">
-				<div id="losses_popup" style="display: none">
-					<div class="popup_body less_transparent_glass_box">
-						<div id="table_div"></div>
-
-						<div class="loss_explanation" id="explanation"></div>
-						<button onclick="close_losses()"><span class="TRANSLATEME_close"></span></button>
-					</div>
-				</div>
-
-				<div id="sources_popup" class="popup" style="display: none;">
-					<div class="popup_body less_transparent_glass_box">
-						<div> 
 <?php
-							$file = file_get_contents("README.md");
-							print(parse_markdown_links(get_string_between($file, "[comment]: <> (BeginSources)", "[comment]: <> (EndSources)")));
+				include("divs/losses_popup.php");
+				include("divs/sources_popup.php");
 ?>
-							<button class="close_button" onclick="close_popup('sources_popup')"><span class="TRANSLATEME_close"></span></button>
-						</div>
-					</div>
-				</div>
-
-
 				<div id="save_dialog" class="popup" style="display: none;">
 					<div class="popup_body less_transparent_glass_box">
 						<div> 
