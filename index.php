129bd462d094ee05744dedd4a71733305760f02f
Norman Koch <norman.koch@tu-dresden.de>
2023-09-25 12:38:15 +0200

    moved stuff from index to cosmo_header

--- index.php
+++ index.php
@@ -152,55 +152,8 @@
 		<meta name="theme-color" content="#7299d2">
 		<meta name="description" content="A tool for creating Neural Networks with TensorFlow.JS without writing a single line of code.">
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-
 <?php
-		if(isset($_GET["start_cosmo"])) {
-			if(isset($_GET["epochs"]) && intval($_GET["epochs"])) {
-?>
-				<script>
-					$(document).ready(async function() {
-						while (!finished_loading) {
-							await delay(200);
-						}
-						await set_epochs(<?php print intval($_GET["epochs"]); ?>);
-					});
-				</script>
-<?php
-			}
-
-			if(isset($_GET["max_iter"]) && intval($_GET["max_iter"])) {
-?>
-				<script>
-					$(document).ready(async function() {
-						while (!finished_loading) {
-							await delay(200);
-						}
-						log("Setting max_activation_iterations to <?php print intval($_GET["max_iter"]); ?>");
-						$("#max_activation_iterations").val(<?php print intval($_GET["max_iter"]); ?>);
-					});
-				</script>
-<?php
-			}
-
-			if(isset($_GET["max_number_of_files_per_category"]) && intval($_GET["max_number_of_files_per_category"])) {
-?>
-				<script>
-					$(document).ready(async function() {
-						while (!finished_loading) {
-							await delay(200);
-						}
-						$("#max_number_of_files_per_category").val(<?php print intval($_GET["max_number_of_files_per_category"]); ?>);
-					});
-				</script>
-<?php
-			}
-
-?>
-			<script>
-				$("#theme_choser").val("light").trigger("change");
-			</script>
-<?php
-		}
+		include("cosmo_header.php");
 ?>
 	</head>
 	<body id='body'>
