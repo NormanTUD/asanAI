df03f704e485c31db929e3d6e98054f0a038eff0
Norman Koch <norman.koch@tu-dresden.de>
2023-09-27 10:18:01 +0200

    fixed set_loss

--- gui.js
+++ gui.js
@@ -1761,7 +1761,7 @@ function set_loss(val, trigger_change = 1) {
 	assert(losses.includes(val), loss + " is not a valid loss. It must be in " + losses.join(", "));
 	assert(typeof (val) == "string", val + " is not an string but " + typeof (val));
 
-	if($("#metric").val() != val) {
+	if($("#loss").val() != val) {
 		$("#loss").val(val);
 		if(trigger_change) {
 			$("#loss").trigger("change");
