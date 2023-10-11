14dc7b0b0d9ff851a4432112cba318da7d5f2952
Norman Koch <norman.koch@tu-dresden.de>
2023-10-11 12:59:57 +0200

    check if pip3 succeeded

--- run_tests.sh
+++ run_tests.sh
@@ -8,7 +8,10 @@ fi
 if [[ ! -d ~/.asanai_test_env ]]; then
 	python3 -mvenv ~/.asanai_test_env
 	source ~/.asanai_test_env/bin/activate
-	pip3 install pyppeteer
+	pip3 install pyppeteer || {
+		echo "pip3 install pyppeteer failed. Are you online?"
+		exit 1
+	}
 fi
 
 source ~/.asanai_test_env/bin/activate
