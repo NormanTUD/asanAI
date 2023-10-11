c3bf4f4520c08918f1156538d6ea458790fbbf58
Norman Koch <norman.koch@tu-dresden.de>
2023-10-11 17:20:10 +0200

    fixed docker

--- install.sh
+++ install.sh
@@ -61,8 +61,7 @@ function install_php {
 	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 	apt-get update
 
-	apt-get install php8.1 php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-intl php8.1-mbstring \
-	php8.1-mysql php8.1-opcache php8.1-readline php8.1-xml php8.1-xsl php8.1-zip php8.1-bz2 libapache2-mod-php8.1 php8.1-bcmath -y
+	apt-get install	libapache2-mod-php8.1 -y
 }
 
 function install_mariadb {
