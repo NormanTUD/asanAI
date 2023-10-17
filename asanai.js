45253e4752319150943f666cb8b4105f80597e19
Norman Koch <norman.koch@tu-dresden.de>
2023-10-17 13:17:15 +0200

    fix

--- asanai.js
+++ asanai.js
@@ -48,6 +48,15 @@ class asanAI {
 		}
 	}
 
+	summary () {
+		if(!this.model) {
+			this.err("No model given. Cannot do summary.");
+			return;
+		}
+
+		this.model.summary();
+	}
+
 	uuidv4() {
 		var res = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
 			(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
