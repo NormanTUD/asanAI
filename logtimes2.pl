87dd316aacdd2b176411a876806b61c3afef35dd
Norman Koch <norman.koch@tu-dresden.de>
2023-11-01 17:56:33 +0100

    fix

---
+++ logtimes2.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use POSIX qw/strftime/;
+use Git::Repository;
+use Text::CSV;
+use DateTime;
+use DateTime::Format::Strptime;
+use Chart::Gnuplot;
+use Data::Dumper;
+
+sub dier {
+	die Dumper \@_;
+}
+
+# Set the Git repository path
+my $repo_path = '.';
+
+# Define the date range (default: last 7 days)
+my $yesterday = DateTime->now->subtract(days => 7);
+my $today = DateTime->now;
+
+# Create a CSV object for writing timetable.csv
+my $csv_timetable = Text::CSV->new({ binary => 1, eol => $/ });
+open my $timetable_fh, '>', 'timetable.csv' or die "Could not open timetable.csv: $!";
+$csv_timetable->print($timetable_fh, ['tag', 'uhrzeit_erster_commit', 'uhrzeit_letzter_commit']);
+
+# Create a CSV object for writing table2.csv
+my $csv_table2 = Text::CSV->new({ binary => 1, eol => $/ });
+open my $table2_fh, '>', 'table2.csv' or die "Could not open table2.csv: $!";
+$csv_table2->print($table2_fh, ['tag', 'arbeitszeit', 'anzahl_commits_an_dem_tag']);
+
+# Initialize total working hours and commits count
+my $total_working_hours = 0;
+my $total_commits_count = 0;
+
+# Initialize Gnuplot data arrays
+my @plot_dates;
+my @plot_commits;
+
+# Iterate through each day in the date range
+while ($yesterday->add(days => 1) <= $today) {
+	my $current_date = $yesterday->strftime('%Y-%m-%d');
+	my $strp = DateTime::Format::Strptime->new(pattern => '%Y-%m-%d');
+	my $start_time = DateTime->new(year => $yesterday->year, month => $yesterday->month, day => $yesterday->day, hour => 0, minute => 0, second => 0);
+	my $end_time = DateTime->new(year => $yesterday->year, month => $yesterday->month, day => $yesterday->day, hour => 23, minute => 59, second => 59);
+
+	my $repo = Git::Repository->new(work_tree => $repo_path);
+	my @commits = $repo->run('log', '--date=local', '--pretty=%at,%h', "--since=$start_time", "--until=$end_time");
+
+	if (@commits) {
+		@commits = reverse @commits;
+		my @commit_times;
+		my $commits_count = 0;
+		foreach my $commit (@commits) {
+			my ($timestamp, $hash) = split(/,/, $commit);
+			my $commit_time = DateTime->from_epoch(epoch => $timestamp);
+			push @commit_times, $commit_time->strftime('%H:%M');
+			$commits_count++;
+		}
+
+		my $first_commit_time = $commit_times[0];
+		my $last_commit_time = $commit_times[-1];
+
+		my $working_hours = $end_time->subtract_datetime($start_time);
+		my $working_hours_formatted = $working_hours->hours . ':' . sprintf("%02d", $working_hours->minutes);
+
+		$total_working_hours += $working_hours->in_units('hours');
+		$total_commits_count += $commits_count;
+
+		$csv_timetable->print($timetable_fh, [$current_date, $first_commit_time, $last_commit_time]);
+		$csv_table2->print($table2_fh, [$current_date, $working_hours_formatted, $commits_count]);
+
+		push @plot_dates, $current_date;
+		push @plot_commits, $commits_count;
+	}
+}
+
+close $timetable_fh;
+close $table2_fh;
+
+# Output total working hours and commits count
+print "Total Working Hours: $total_working_hours hours\n";
+print "Total Commits Count: $total_commits_count\n";
+
+# Use Chart::Gnuplot to create a plot
+my $chart = Chart::Gnuplot->new(
+	output => 'commits_plot.png',
+	title  => 'Commits per Day',
+	xdata  => 'time',
+	xlabel => 'Date',
+	ylabel => 'Commits'
+);
+$chart->set('timefmt' => '%Y-%m-%d', 'format x' => '%b %d');
+$chart->plot(
+	Chart::Gnuplot::DataSet->new(
+		xdata => \@plot_dates,
+		ydata => \@plot_commits,
+		title => 'Commits',
+		style => 'linespoints',
+	)
+);
+
+# Clean up temporary files
+#unlink 'timetable.csv';
+#unlink 'table2.csv';
