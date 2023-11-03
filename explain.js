4148217e22bba8ff4a73fa28dd4d4916a8a7904f
Norman Koch <norman.koch@tu-dresden.de>
2023-11-03 16:37:06 +0100

    fixed gradcam

--- explain.js
+++ explain.js
@@ -2822,6 +2822,7 @@ async function grad_class_activation_map(model, x, class_idx, overlay_factor = 2
 		if(("" + e).includes("already disposed")) {
 			dbg("Model weights are disposed. Probably the model was recompiled during prediction");
 		} else {
+			err("ERROR in next line stack:", e.stack);
 			await write_error(e);
 		}
 		return null;
