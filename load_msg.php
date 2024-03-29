38ef436bc18b5f207f9b9c95d14a38b307e68184
Norman Koch <norman.koch@tu-dresden.de>
2024-03-29 15:22:05 +0100

    fix

--- load_msg.php
+++ load_msg.php
@@ -1,6 +1,22 @@
 <div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw; pointer-events: none; background-color: white; user-select: none;" id="loading_icon_wrapper">
 	<img src="_gui/scads_logo.svg" class="invert_in_dark_mode" alt="Loading..." style="position: absolute; left: 10px; top: 10px; height: 8vw">
-	<img src="_gui/logo.svg" class="invert_in_dark_mode" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
+<?php
+	if(isset($_COOKIE["theme"])) {
+		if($_COOKIE["theme"] == "darkmode") {
+?>
+			<img src="_gui/logo_small_dark.png" class="invert_in_dark_mode" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
+<?php
+		} else {
+?>
+			<img src="_gui/logo.svg" class="invert_in_dark_mode" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
+<?php
+		}
+	} else {
+?>
+		<img src="_gui/logo.svg" class="invert_in_dark_mode" alt="Loading..." style="position: absolute; right: 10px; top: 10px; height: 8vw">
+<?php
+	}
+?>
 	<img src="_gui/loading_favicon.gif" alt="Loading..." style="max-width: 20vw;">
 	<br>
 	<div id="load_msg"></div>
