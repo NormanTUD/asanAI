5c4b090084ddbbd02ebb66653aa76f8fcafdca60
Norman Koch <norman.koch@tu-dresden.de>
2024-05-02 11:01:24 +0200

    fix

--- presentation/index.php
+++ presentation/index.php
@@ -158,10 +158,28 @@
 			var max_page = get_max_page();
 
 			$("#max_page_nr").html(max_page);
+
+			$("#page_nr").html(get_current_page());
+		}
+
+		function get_current_page() {
+			var page_id = null;
+			$(".folie").each(function (i, e) { 
+				if($(e).is(":visible")) {
+					page_id = i;
+				}
+			});
+
+			return page_id;
 		}
 
 		function show_folie_nr(i)  {
 			$(".folie").hide();
+
+			if(i < 0) {
+				i = 0;
+			}
+
 			if(i >= get_max_page()) {
 				i = 0;
 			}
@@ -173,9 +191,25 @@
 			}
 		}
 
-		$(document).ready(function() {
+		function show_prev_folie() {
+			var current = get_current_page();
+
+			show_folie_nr(current - 1);
+
 			set_page_footer();
+		}
+
+		function show_next_folie() {
+			var current = get_current_page();
+
+			show_folie_nr(current + 1);
+
+			set_page_footer();
+		}
+
+		$(document).ready(function() {
 			show_folie_nr(0)
+			set_page_footer();
 		});
 	</script>
 </html>
