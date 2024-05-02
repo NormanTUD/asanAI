09dcd15305514b1e820f9245a53cd0293549f013
Norman Koch <norman.koch@tu-dresden.de>
2024-05-02 10:38:10 +0200

    fix

--- presentation/1.php
+++ presentation/1.php
@@ -1,5 +1,7 @@
-<h1>Hallo</h1>
-<div>
-	<li>hallo welt</li>
-	<li>hallo welt</li>
+<div class="folie">
+	<h1>Hallo</h1>
+	<div>
+		<li>hallo welt</li>
+		<li>hallo welt</li>
+	</div>
 </div>
--- presentation/index.php
+++ presentation/index.php
@@ -48,38 +48,54 @@
 				top: 0px;
 			}
 
-			#folie {
+			.folie {
 				margin: 12vw;
 			}
 
 			.footer_logo {
-				max-width: 50%;
+				max-width: 80%;
 			}
 
 			.a_fourth {
 				width: 25%;
+				text-align: center;
 			}
 		</style>
+		<script src='../libs/jquery.js' crossorigin></script>
 	</head>
 	<body>
 		<div id="backgrounds">
 			<img id="topleft_background" src="new_graphics/topleft_design.png" />
 			<img id="bubble_background" src="new_graphics/Loops_Bubble_2.png" />
 		</div>
-		<div id="folie">
 <?php
-			include("1.php");
+		include("1.php");
+		include("1.php");
+		include("1.php");
 ?>
-		</div>
 		<div id="footer">
 			<table style='width: 100%'>
 				<tr>
 					<td class="a_fourth"><img class="footer_logo" src="new_graphics/logo/ScaDSAI_logo.png"></td>
-					<td class="a_fourth">A</td>
+					<td class="a_fourth"><div id="shorttitle"></div><div id="page_nr">1</div>/<div id="max_page_nr">1</div></td>
 					<td class="a_fourth"><img class="footer_logo" src="new_graphics/logo/TU_Dresden_Logo_blau_HKS41.png"></td>
 					<td class="a_fourth"><img class="footer_logo" src="new_graphics/logo/UniLeipzig-Logo-Neu_clean.png"></td>
 				</tr>
 			</table>
 		</div>
 	</body>
+
+	<script>
+		function get_max_page () {
+			return $(".folie").length;
+		}
+
+		function set_page_footer () {
+			var max_page = get_max_page();
+
+			$("#max_page_nr").html(max_page);
+		}
+
+		set_page_footer();
+	</script>
 </html>
