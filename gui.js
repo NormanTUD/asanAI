c88ccea7cdb24e6459ec4875782692472a72a3aa
Norman Koch <norman.koch@tu-dresden.de>
2024-08-28 10:05:55 +0200

    dont allow negative radius

--- gui.js
+++ gui.js
@@ -8353,28 +8353,27 @@ function _draw_neurons_or_conv2d(layerId, numNeurons, ctx, verticalSpacing, laye
 				}
 			}
 
-			if(this_layer_output) {
-				var minVal = Math.min(...this_layer_output);
-				var maxVal = Math.max(...this_layer_output);
+			var radius = Math.min(maxShapeSize, availableSpace);
+			if(radius) {
+				if(this_layer_output) {
+					var minVal = Math.min(...this_layer_output);
+					var maxVal = Math.max(...this_layer_output);
 
-				var value = this_layer_output[j];
-				var normalizedValue = Math.floor(((value - minVal) / (maxVal - minVal)) * 255);
+					var value = this_layer_output[j];
+					var normalizedValue = Math.floor(((value - minVal) / (maxVal - minVal)) * 255);
 
-				ctx.fillStyle = `rgb(${normalizedValue}, ${normalizedValue}, ${normalizedValue})`;
+					ctx.fillStyle = `rgb(${normalizedValue}, ${normalizedValue}, ${normalizedValue})`;
 
-				// Adjust the radius based on available vertical space
-				var availableSpace = verticalSpacing / 2 - 2; // Subtracting 2 for a small margin
-				var radius = Math.min(maxShapeSize, availableSpace);
-				ctx.arc(layerX, neuronY, radius, 0, 2 * Math.PI);
-			} else {
-				var availableSpace = verticalSpacing / 2 - 2;
-				var radius = Math.min(maxShapeSize, availableSpace);
-				if(radius) {
+					// Adjust the radius based on available vertical space
+					var availableSpace = verticalSpacing / 2 - 2; // Subtracting 2 for a small margin
 					ctx.arc(layerX, neuronY, radius, 0, 2 * Math.PI);
-					ctx.fillStyle = "white";
 				} else {
-					log_once("Found negative radius!");
+					var availableSpace = verticalSpacing / 2 - 2;
+						ctx.arc(layerX, neuronY, radius, 0, 2 * Math.PI);
+						ctx.fillStyle = "white";
 				}
+			} else {
+				log_once("Found negative radius!");
 			}
 
 			ctx.strokeStyle = "black";
