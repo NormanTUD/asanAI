8da797687e7465a140800f3af066161f4c918c25
Norman Koch <norman.koch@tu-dresden.de>
2024-09-05 15:04:11 +0200

    fix

--- run_tests.sh
+++ run_tests.sh
@@ -5,14 +5,17 @@ if ! command -v python3 2>/dev/null >/dev/null; then
 	exit 1
 fi
 
+echo "====== Starting docker container ======"
 bash docker.sh --local-port 1122
 exit_code=$?
+echo "====== Started docker container ======"
 
 if [[ "$exit_code" -ne 0 ]]; then
 	echo "Could not start docker"
 	exit 255
 fi
 
+echo "====== Checking virtualenv ======"
 if [[ ! -d ~/.asanai_test_env ]]; then
 	python3 -mvenv ~/.asanai_test_env
 	source ~/.asanai_test_env/bin/activate
@@ -24,6 +27,7 @@ if [[ ! -d ~/.asanai_test_env ]]; then
 fi
 
 source ~/.asanai_test_env/bin/activate
+echo "====== Checked virtualenv ======"
 
 python3 _run_tests.py
 
