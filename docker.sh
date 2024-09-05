4f5eb6c0a30a276b7b6cfb8f7baad4f87ba94469
Norman Koch <norman.koch@tu-dresden.de>
2024-09-05 14:14:23 +0200

    fix

--- docker.sh
+++ docker.sh
@@ -74,7 +74,7 @@ if ! command -v docker &>/dev/null; then
 fi
 
 if ! command -v docker-compose 2>&1 >/dev/null; then
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
 fi
 
 if ! command -v wget &>/dev/null; then
