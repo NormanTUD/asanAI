a5e4a4f516c6e2e42544ddb42489753093d50301
Norman Koch <norman.koch@tu-dresden.de>
2024-12-17 10:31:59 +0100

    fix

--- tabs/code.php
+++ tabs/code.php
@@ -13,6 +13,7 @@
 				<span class="TRANSLATEME_copy_to_clipboard"></span>
 			</button>
 			<button onclick="save_model()">
+				<img src="_gui/icons/download.svg" class="icon_small" />
 				<span class="TRANSLATEME_download_model_data"></span>
 			</button>
 		</span>
@@ -29,6 +30,7 @@
 				</span>
 			</button>
 			<button onclick="save_model()">
+				<img src="_gui/icons/download.svg" class="icon_small" />
 				<span class="TRANSLATEME_download_model_data"></span>
 			</button>
 			<button id="download_with_data" onclick="download_model_for_training()">
