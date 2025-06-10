9ce8bb90c68ea2c7a18da79f9a8d746c720bc6cb
n <norman.koch@tu-dresden.de>
2025-06-10 17:24:45 +0200

    matomo

--- index.php
+++ index.php
@@ -141,6 +141,13 @@
 				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 				g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 			})();
+
+			var _mtm = window._mtm = window._mtm || [];
+			_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+			(function() {
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.async=true; g.src='https://asanai.scads.ai/matomo/js/container_3UHSZzXd.js'; s.parentNode.insertBefore(g,s);
+			})();
 		</script>
 
 		<link rel="stylesheet" href="./libs/temml/Temml-Local.css">
