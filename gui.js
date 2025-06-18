f64359ec669d8a6ecf4a463dc76e122a2f804794
Norman Koch <norman.koch@tu-dresden.de>
2025-06-18 12:18:33 +0200

    docker installer

--- gui.js
+++ gui.js
@@ -1549,6 +1549,8 @@ function python_boilerplate (input_shape_is_image_val, _expert_mode=0) {
 	python_code += "# - source asanaienv/bin/activate\n";
 	python_code += "# - pip3 install tf-nightly protobuf";
 
+	python_code += `# If tensorflow_converter fails, install Docker from https://docs.docker.com/get-docker/\n`;
+
 	if (input_shape_is_image_val) {
 		python_code += " scikit-image opencv-python";
 	}
