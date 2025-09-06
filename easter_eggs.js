abd0db3529b6c0cf73abc3c9d1c93fb79bc8a6b0
n <norman.koch@tu-dresden.de>
2025-09-06 17:09:25 +0200

    less long func

--- easter_eggs.js
+++ easter_eggs.js
@@ -34,103 +34,103 @@ function randomInRange(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
+function confetti_a () {
+	confetti({
+		particleCount: 100,
+		spread: 70,
+		origin: { y: 0.6 }
+	});
+}
+
+function confetti_b () {
+	confetti({
+		angle: randomInRange(55, 125),
+		spread: randomInRange(50, 70),
+		particleCount: randomInRange(50, 100),
+		origin: { y: 0.6 }
+	});
+}
+
+function confetti_c() {
+	var count = 200;
+	var defaults = {
+		origin: { y: 0.7 }
+	};
+
+	function fire(particleRatio, opts) {
+		confetti(Object.assign({}, defaults, opts, {
+			particleCount: Math.floor(count * particleRatio)
+		}));
+	}
+
+	fire(0.25, {
+		spread: 26,
+		startVelocity: 55,
+	});
+	fire(0.2, {
+		spread: 60,
+	});
+	fire(0.35, {
+		spread: 100,
+		decay: 0.91,
+		scalar: 0.8
+	});
+	fire(0.1, {
+		spread: 120,
+		startVelocity: 25,
+		decay: 0.92,
+		scalar: 1.2
+	});
+	fire(0.1, {
+		spread: 120,
+		startVelocity: 45,
+	});
+}
+
+function confetti_d () {
+	var defaults = {
+		spread: 360,
+		ticks: 50,
+		gravity: 0,
+		decay: 0.94,
+		startVelocity: 30,
+		shapes: ["star"],
+		colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"]
+	};
+
+	function shoot() {
+		confetti({
+			...defaults,
+			particleCount: 40,
+			scalar: 1.2,
+			shapes: ["star"]
+		});
+
+		confetti({
+			...defaults,
+			particleCount: 10,
+			scalar: 0.75,
+			shapes: ["circle"]
+		});
+	}
+
+	setTimeout(shoot, 0);
+	setTimeout(shoot, 100);
+	setTimeout(shoot, 200);
+}
+
+async function confetti_e () {
+	$(".fireworks-container").show();
+	in_fireworks = true;
+	var fw = new Fireworks(document.querySelector(".fireworks-container"));
+	fw.start();
+	await delay(10000);
+	fw.stop();
+	in_fireworks = false;
+	$(".fireworks-container").html("").hide();
+}
+
 function _confetti () {
-	function confetti_a () {
-		confetti({
-			particleCount: 100,
-			spread: 70,
-			origin: { y: 0.6 }
-		});
-	}
-
-	function confetti_b () {
-		confetti({
-			angle: randomInRange(55, 125),
-			spread: randomInRange(50, 70),
-			particleCount: randomInRange(50, 100),
-			origin: { y: 0.6 }
-		});
-	}
-
-	function confetti_c() {
-		var count = 200;
-		var defaults = {
-			origin: { y: 0.7 }
-		};
-
-		function fire(particleRatio, opts) {
-			confetti(Object.assign({}, defaults, opts, {
-				particleCount: Math.floor(count * particleRatio)
-			}));
-		}
-
-		fire(0.25, {
-			spread: 26,
-			startVelocity: 55,
-		});
-		fire(0.2, {
-			spread: 60,
-		});
-		fire(0.35, {
-			spread: 100,
-			decay: 0.91,
-			scalar: 0.8
-		});
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 25,
-			decay: 0.92,
-			scalar: 1.2
-		});
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 45,
-		});
-	}
-
-	function confetti_d () {
-		var defaults = {
-			spread: 360,
-			ticks: 50,
-			gravity: 0,
-			decay: 0.94,
-			startVelocity: 30,
-			shapes: ["star"],
-			colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"]
-		};
-
-		function shoot() {
-			confetti({
-				...defaults,
-				particleCount: 40,
-				scalar: 1.2,
-				shapes: ["star"]
-			});
-
-			confetti({
-				...defaults,
-				particleCount: 10,
-				scalar: 0.75,
-				shapes: ["circle"]
-			});
-		}
-
-		setTimeout(shoot, 0);
-		setTimeout(shoot, 100);
-		setTimeout(shoot, 200);
-	}
-
-	async function confetti_e () {
-		$(".fireworks-container").show();
-		in_fireworks = true;
-		var fw = new Fireworks(document.querySelector(".fireworks-container"));
-		fw.start();
-		await delay(10000);
-		fw.stop();
-		in_fireworks = false;
-		$(".fireworks-container").html("").hide();
-	}
-
 	const confettiFunctions = [confetti_a, confetti_b, confetti_c, confetti_d, confetti_e];
 	const randomIndex = Math.floor(Math.random() * confettiFunctions.length);
 	const randomConfettiFunction = confettiFunctions[randomIndex];
