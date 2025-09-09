5778e874dfc4138edb9d17eed0b465f7b40eaf0b
n <norman.koch@tu-dresden.de>
2025-09-09 13:07:50 +0200

    added asanai bug msg

--- explain.js
+++ explain.js
@@ -734,7 +734,7 @@ function explain_error_msg (_err) {
 			explanation = "Try choosing another loss and metric function, like Mean Squared Error (MSE) or Mean Absolute Error (MAE).";
 		} else if(_err.includes("Cannot find a connection between any variable and the result of the loss function")) {
 			explanation = "This is probably a bug in asanAI. This may happen when the function run_neural_network is called, but the model is not compiled (e.g. the compile_model function throws an exception). You should never see this. Sorry.";
-		} else if (_err.includes("Input Tensors should have the same number of samples as target Tensors") || _err.includes("not defined")) {
+		} else if (_err.includes("Input Tensors should have the same number of samples as target Tensors") || _err.includes("not defined") || _err.includes("Cannot convert")) {
 			explanation = "This is probably a bug in asanAI"
 		} else if(_err.includes("numeric tensor, but got string tensor")) {
 			if($("#data_origin").val() == "csv") {
