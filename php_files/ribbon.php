766d82ed7b934d6d43ac2f4d27a308b8c3ebc366
n <norman.koch@tu-dresden.de>
2025-09-13 11:59:14 +0200

    removed undo redo button html

--- php_files/ribbon.php
+++ php_files/ribbon.php
@@ -3,8 +3,6 @@
 		<li><span class="symbol_button" title="Hide Ribbon" onclick="hide_ribbon()" style='cursor: pointer; color: gray'>&#9776;</span></li>
 		<li><span class="symbol_button" title="Download model" style="cursor: pointer" onclick="download_model_and_weights_and_labels()"><img class="ribbon_icon" src="_gui/icons/floppy.svg" /></span></li>
 		<li><span class="symbol_button" title="Upload model" id="upload_file_dialog" onclick="open_upload_dialog()" style="cursor: pointer"><img class="ribbon_icon" src="_gui/icons/folder.svg" /></span></li>
-		<li><span class="symbol_button disabled_symbol undo_redo_button" title="Undo last action" id="undo_button" onclick="undo()"><img class="ribbon_icon" src="_gui/icons/undo.svg" /></span></li>
-		<li><span class="symbol_button disabled_symbol undo_redo_button" title="Redo last undone action" id="redo_button" onclick="redo()"><img class="ribbon_icon" src="_gui/icons/redo.svg" /></span></li>
 		<li><span id="custom_webcam_training_data_small" title="Start webcam for acquiring custom images" style="display: none" class="enabled_symbol only_when_webcam input_shape_is_image symbol_button" onclick="set_custom_webcam_training_data()"><img class="ribbon_icon" src="_gui/icons/webcam.svg" /></span></li>
 		<li><span id="custom_image_training_data_small" title="Set custom images for training" style="display: none" class="only_when_webcam enabled_symbol input_shape_is_image symbol_button" onclick="set_custom_image_training()"><img class="ribbon_icon" src="_gui/icons/photos.svg" /></span></li>
 		<li><span class="symbol_button" title="Help" style="cursor: help" id="manual_page_link" onclick="window.open('manual.html', '_blank').focus();"><img class="ribbon_icon" src="_gui/icons/help.svg" /></span></li>
