70d1844952c5841d87cfa2adccfe86749a7988d3
Norman Koch <norman.koch@tu-dresden.de>
2025-10-09 11:12:26 +0200

    respond_with_error

--- api/save_error_log.php
+++ api/save_error_log.php
@@ -149,10 +149,11 @@
 
 	try {
 		receiveAndCheckHTML($log_file_dir, $html_code);
-		if(count($errors) == 0) {
+		if($errors && count($errors) == 0) {
 			http_response_code(200);
 			echo json_encode(["success" => true]);
 		}
+		respond_with_error("No errors found", 500);
 	} catch (Exception $e) {
 		respond_with_error($e->getMessage(), 500);
 	}
