29dad6dcd33d72d7ae7b5d570959e1de1aae54c6
Norman Koch <norman.koch@tu-dresden.de>
2025-10-09 08:01:58 +0200

    fix

--- math_mode.js
+++ math_mode.js
@@ -1430,8 +1430,16 @@ function get_gaussian_dropout_latex (layer_idx) {
 function get_average_pooling_2d_latex (layer_idx) {
 	const _h = _get_h(layer_idx);
 	const _h_next = _get_h(layer_idx + 1);
-	const pool_size_x = parse_int(get_item_value(layer_idx, "pool_size_x"));
-	const pool_size_y = parse_int(get_item_value(layer_idx, "pool_size_y"));
+
+	var pool_size_x = get_item_value(layer_idx, "pool_size_x");
+	var pool_size_y = get_item_value(layer_idx, "pool_size_y");
+
+	if(!looks_like_number(pool_size_x) || !looks_like_number(pool_size_y)) {
+		return `\\text{Invalid settings for this layer}`;
+	}
+
+	pool_size_x = parse_int(pool_size_x);
+	pool_size_y = parse_int(pool_size_y);
 
 	return `${_h_next} = \\frac{1}{N \\times M} \\sum_{i=1}^{N = ${pool_size_x}} \\sum_{j=1}^{M = ${pool_size_y}} ${_h} \\left(x + i, y + j\\right) \\\\`;
 }
