2e533d0d02d7a95bdc5169ef7251997944998d78
n <norman.koch@tu-dresden.de>
2025-10-11 12:04:28 +0200

    renamed for smoke test

--- easter_eggs.js
+++ easter_eggs.js
@@ -59,31 +59,31 @@ function confetti_c() {
 		origin: { y: 0.7 }
 	};
 
-	function fire(particleRatio, opts) {
+	function confetti_fire(particleRatio, opts) {
 		confetti(Object.assign({}, defaults, opts, {
 			particleCount: Math.floor(count * particleRatio)
 		}));
 	}
 
-	fire(0.25, {
+	confetti_fire(0.25, {
 		spread: 26,
 		startVelocity: 55,
 	});
-	fire(0.2, {
+	confetti_fire(0.2, {
 		spread: 60,
 	});
-	fire(0.35, {
+	confetti_fire(0.35, {
 		spread: 100,
 		decay: 0.91,
 		scalar: 0.8
 	});
-	fire(0.1, {
+	confetti_fire(0.1, {
 		spread: 120,
 		startVelocity: 25,
 		decay: 0.92,
 		scalar: 1.2
 	});
-	fire(0.1, {
+	confetti_fire(0.1, {
 		spread: 120,
 		startVelocity: 45,
 	});
