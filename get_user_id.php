1002f4442e61511ac467f458bccf884af83903ab
n <norman.koch@tu-dresden.de>
2025-10-20 15:32:22 +0200

    removed old files

--- get_user_id.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include('function.php');
-
-	if(array_key_exists("session_id", $_GET)) {
-		$session_id = $_GET["session_id"];
-		print get_user_id_from_session_id($session_id);
-	}
-?>
--- php_files/get_training_data.php
+++ php_files/get_training_data.php
@@ -9,7 +9,6 @@
 	$model_id = $_GET["id"];
 
 	$model_is_public = model_is_public($model_id);
-	$user_id = get_user_id_from_session_id($_COOKIE["session_id"]);
 	$user_is_admin = is_admin() ? 1 : 0;
 	$can_edit_model = can_edit_model($model_id);
 
