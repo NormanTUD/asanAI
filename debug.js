fc5ec31e1e1db94288cb75d4c0b625fe7599027c
Norman Koch <norman.koch@tu-dresden.de>
2025-10-30 12:18:29 +0100

    show stack trace in log

--- debug.js
+++ debug.js
@@ -122,12 +122,16 @@ function wrn (...args) {
 		console.trace();
 	}
 
+	const stack_trace = get_stack_trace(1);
+
 	var struct = {
 		"type": "warn",
-		"stacktrace": get_stack_trace(1),
+		"stacktrace": stack_trace,
 		"log": args,
 		"time": parse_int(Date.now() / 1000)
 	};
+
+	console.log(`Stack-Trace: ${stack_trace}`);
 
 	_full_debug_log.push(struct);
 
