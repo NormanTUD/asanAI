ead178f547dab0538f0863708052a0ac0e569548
Norman Koch <norman.koch@tu-dresden.de>
2025-11-10 10:54:05 +0100

    fix

--- snake_activation_layer.js
+++ snake_activation_layer.js
@@ -5,71 +5,76 @@
  * alpha is a learnable parameter.
  */
 class Snake extends tf.layers.Layer {
-    constructor(config) {
-        super(config);
+	constructor(config) {
+		super(config);
 
-        // Initial value for alpha
-        this.alpha = config.alpha || 1;
+		// Store initial alpha value as a number
+		this.alphaVal = config.alpha || 1;
 
-        // Whether alpha should be trainable
-        this.trainable = config.trainable !== undefined ? config.trainable : true;
-    }
+		// Whether alpha should be trainable
+		this.trainable = config.trainable !== undefined ? config.trainable : true;
+	}
 
-    /**
-     * build() is called when the layer is connected to upstream layers.
-     * This is where trainable weights are created.
-     */
-    build(inputShape) {
-        // Create the trainable alpha variable
-        this.alpha = this.addWeight(
-            "alpha",
-            [],                  // scalar
-            "float32",
-            tf.initializers.constant({ value: this.alpha }),
-            null,
-            this.trainable
-        );
+	/**
+	 * build() is called when the layer is connected to upstream layers.
+	 * This is where trainable weights are created.
+	 */
+	build(inputShape) {
+		// Create the trainable alpha variable (scalar)
+		this.alpha = this.addWeight(
+			"alpha",
+			[],                   // scalar
+			"float32",
+			tf.initializers.constant({ value: this.alphaVal }),
+			null,
+			this.trainable
+		);
 
-        super.build(inputShape);
-    }
+		super.build(inputShape);
+	}
 
-    /**
-     * call() computes x + (sin(x)^2 / alpha)
-     */
-    call(input, kwargs) {
-        return tf.tidy(() => {
-            const x = input[0] || input; // support both Tensor or [Tensor]
-            const sinX = tf.sin(x);
-            const sinX2 = tf.square(sinX);
-            const out = tf.add(x, tf.div(sinX2, this.alpha.read()));
-            return out;
-        });
-    }
+	/**
+	 * call() computes x + (sin(x)^2 / alpha)
+	 */
+	call(input, kwargs) {
+		return tf.tidy(() => {
+			// Ensure input is a Tensor
+			const x = input instanceof Array ? input[0] : input;
 
-    /**
-     * getConfig() for serialization
-     */
-    getConfig() {
-        const baseConfig = super.getConfig();
-        const config = Object.assign({}, baseConfig, {
-            alpha: this.alpha,
-            trainable: this.trainable
-        });
-        return config;
-    }
+			// sin^2(x)
+			const sinX2 = tf.square(tf.sin(x));
 
-    /**
-     * Required by tf.serialization.registerClass
-     */
-    static get className() {
-        return "Snake";
-    }
+			// x + sin^2(x) / alpha
+			const out = tf.add(x, tf.div(sinX2, this.alpha.read()));
+
+			return out;
+		});
+	}
+
+	/**
+	 * getConfig() for serialization
+	 */
+	getConfig() {
+		const baseConfig = super.getConfig();
+		const config = Object.assign({}, baseConfig, {
+			alpha: this.alphaVal,
+			trainable: this.trainable
+		});
+		return config;
+	}
+
+	/**
+	 * Required by tf.serialization.registerClass
+	 */
+	static get className() {
+		return "Snake";
+	}
 }
 
 // Register the custom layer
 tf.serialization.registerClass(Snake);
 
-// Factory-Funktion für tf.layers
+// Factory function for tf.layers
 tf.layers.Snake = function(config) {
-    return new Snake(config);
+	return new Snake(config);
 };
