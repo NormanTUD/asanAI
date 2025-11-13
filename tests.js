054757616a6c525fd728004fe59175f17f8f3dcc
n <norman.koch@tu-dresden.de>
2025-11-13 14:06:08 +0100

    added more test

--- tests.js
+++ tests.js
@@ -1905,23 +1905,49 @@ async function test_if_functions_work_as_expected () {
 
 	await delay(1000);
 
-	$(".activation").val("linear").trigger("change")
+	var has_error = false;
 
-	await delay(1000);
+	const wanted_results = {
+		"linear": 1,
+		"relu": 1,
+		"relu6": 1,
+		"selu": 1.0507010221481323,
+		"elu": 1,
+		"softplus": 1.31326162815094,
+		"softsign": 0.5,
+		"softmax": 1,
+		"tanh": 0.7615941166877747,
+		"leakyrelu": 1,
+		"sigmoid": 0.7310585975646973
+	};
 
-	var predict_one = array_sync(model.predict(tensor([1])))
+	for (const actfun of Object.keys(wanted_results)) {
+		$(".activation").val(actfun).trigger("change")
 
-	var predict_one_shape = get_shape_from_array(predict_one)
+		await wait_for_updated_page(3);
 
-	var stringified_predict_one_shape = JSON.stringify(predict_one_shape);
+		await delay(1000);
 
-	if(stringified_predict_one_shape !=  "[1,1]") {
-		err(`stringified_predict_one_shape is not [1,1], but ${stringified_predict_one_shape}`);
-		return false;
+		var predict_res = model.predict(tensor([1]));
+
+		var predict_one = array_sync(predict_res)
+
+		var predict_one_shape = get_shape_from_array(predict_one)
+
+		var stringified_predict_one_shape = JSON.stringify(predict_one_shape);
+
+		if(stringified_predict_one_shape != "[1,1]") {
+			err(`stringified_predict_one_shape is not [1,1], but ${stringified_predict_one_shape} (actfun: ${actfun})`);
+			has_error = true;
+		}
+
+		if(predict_one[0][0] != wanted_results[actfun]) {
+			err(`Model should have gotten ${wanted_results[actfun]} but got ${predict_one[0][0]} (actfun: ${actfun})`);
+			has_error = true;
+		}
 	}
 
-	if(predict_one[0][0] != 1) {
-		err(`Model should have gotten 1 but got ${predict_one[0][0]}`);
+	if(has_error) {
 		return false;
 	}
 
