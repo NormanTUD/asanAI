0df03758b55932efcf75d0b220d1c10cb1f4b55d
n <norman.koch@tu-dresden.de>
2025-11-16 16:45:15 +0100

    removed alexnet

--- visualizations/util.js
+++ visualizations/util.js
@@ -1,13 +1,5 @@
 "use strict";
 
-let nWise = (n, array) => {
-	var iterators = Array(n).fill().map(() => array[Symbol.iterator]());
-	iterators.forEach((it, index) => Array(index).fill().forEach(() => it.next()));
-	return Array(array.length - n + 1).fill().map(() => (iterators.map(it => it.next().value)));
-};
-
-let pairWise = (array) => nWise(2, array);
-
 let sum = (arr) => arr.reduce((a,b)=>a+b);
 
 let range = n => [...Array(n).keys()];
