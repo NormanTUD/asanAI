f08074f7f72f284bdccf3035a1d8334869e607ab
n <norman.koch@tu-dresden.de>
2025-11-30 12:12:03 +0100

    fix

--- tests.js
+++ tests.js
@@ -703,7 +703,7 @@ async function run_super_quick_tests (quick=0) {
 
 	test_equal("array_to_fixed([1.555,2.555,3.555], 2)", JSON.stringify(array_to_fixed([1.555,2.555,3.555], 2)), "[1.55,2.56,3.56]");
 
-	test_equal("group_layers([ \"conv2d\", \"maxPooling2d\", \"conv2d\", \"maxPooling2d\", \"flatten\", \"dropout\", \"dense\", \"dense\" ])", JSON.stringify(group_layers([ "conv2d", "maxPooling2d", "conv2d", "maxPooling2d", "flatten", "dropout", "dense", "dense" ])), "[{\"<span class='TRANSLATEME_feature_extraction'></span>\":[0,1,2,3]},{\"<span class='TRANSLATEME_flatten'></span>\":[4]},{\"Feature ex&shy;trac&shy;tion &amp; Over&shy;fit&shy;ting pre&shy;vention\":[5]},{\"<span class='TRANSLATEME_classification'></span>\":[6,7]}]");
+	test_equal("group_layers([ \"conv2d\", \"maxPooling2d\", \"conv2d\", \"maxPooling2d\", \"flatten\", \"dropout\", \"dense\", \"dense\" ])", JSON.stringify(group_layers([ "conv2d", "maxPooling2d", "conv2d", "maxPooling2d", "flatten", "dropout", "dense", "dense" ])),  `[{"<span class='TRANSLATEME_feature_extraction'></span>":[0,1,2,3]},{"<span class='TRANSLATEME_flatten'></span>":[4]},{"<span class='TRANSLATEME_feature_extraction_and_overfitting_prevention'></span>":[5]},{"<span class='TRANSLATEME_classification'></span>":[6,7]}]`);
 
 	test_equal("decille([1,2,3,4,5,6,7,8,9,10, 11], 1)", decille([1,2,3,4,5,6,7,8,9,10, 11], 1), 10);
 
