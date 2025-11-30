35fde13e9d5a1ad30ded1865fcb2b5babe0cadb4
n <norman.koch@tu-dresden.de>
2025-11-30 15:20:16 +0100

    get_loss_landscape_plot_data

--- loss_landscape.js
+++ loss_landscape.js
@@ -31,3 +31,56 @@ function get_loss_from_data(m, input, wanted) {
 
 	return array_sync_if_tensor(loss)[0];
 }
+
+function get_loss_landscape_plot_data(m, input, wanted, steps) {
+	if (!Object.keys(m).includes("layers")) {
+		info("Model has no layers")
+		return null;
+	}
+
+	if(m.layers.length != 1) {
+		info("Can currently only plot one layer");
+		return null;
+	}
+
+	if(!model?.layers[0]?.weights?.length == 2) {
+		info("First and only layer must have 2 weights (weight/bias)")
+		return null;
+	}
+
+	const original_weights = array_sync_if_tensor(model.layers[0].weights[0].val);
+	if(get_shape_from_array_or_tensor(original_weights).length != 1) {
+		info("Can only plot weight shape of length 1");
+		return null;
+	}
+
+	const weight = original_weights[0];
+
+	const original_bias = array_sync_if_tensor(model.layers[0].weights[1].val);
+	if(get_shape_from_array_or_tensor(original_bias).length != 1) {
+		info("Can only plot bias shape of length 1");
+		return null;
+	}
+
+	const bias = original_bias[0];
+
+	const weight_distance = weight == 0 ? 1 : (weight);
+	const bias_distance = bias == 0 ? 1 : (bias);
+
+	const min_weight = weight - weight_distance;
+	const max_weight = weight + weight_distance;
+
+	const min_bias = bias - bias_distance;
+	const max_bias = bias + bias_distance;
+
+	const weight_stepsize = Math.abs(min_weight - max_weight) / steps;
+	const bias_stepsize = Math.abs(min_bias - max_bias) / steps;
+
+	for (var step_bias = 0; step_bias < steps; step_bias++) {
+		for (var step_weight = 0; step_weight < steps; step_weight++) {
+			const this_bias = min_bias + (step_bias * bias_stepsize);
+			const this_weight = min_weight + (step_weight * weight_stepsize);
+
+		}
+	}
+}
