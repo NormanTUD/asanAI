293501e9c8e927ac96c33b356749d4ed55b0b364
Norman Koch <norman.koch@tu-dresden.de>
2025-12-02 11:10:42 +0100

    splitted alpha and beta for snake layer

--- math_mode.js
+++ math_mode.js
@@ -1636,12 +1636,16 @@ function get_snake_layer_latex (layer_idx) {
 	var alpha = model?.layers[layer_idx]?.weights[0]?.val;
 	var beta = model?.layers[layer_idx]?.weights[1]?.val;
 
-	if(alpha === undefined || alpha === null || alpha == "" || beta === undefined || beta === null || beta == "") {
-		return "\\text{Cannot determine alpha for snake layer}";
-	} else {
-		alpha = array_sync(alpha);
-		beta = array_sync(beta);
+	if(alpha === undefined || alpha === null || alpha == "") {
+		return "\\text{Cannot determine } \\alpha \\text{ for snake layer}";
 	}
+
+	if(beta === undefined || beta === null || beta == "") {
+		return "\\text{Cannot determine } \\beta \\text{ for snake layer}";
+	}
+
+	alpha = array_sync(alpha);
+	beta = array_sync(beta);
 
 	return `${_h_next} = ${beta} \\left(${_h} + \\frac{\\sin^2\(${alpha} \\cdot ${_h} \)}{${alpha}}\\right)`;
 }
