1cbb1f7f009ed0a457dca126932192385833ba80
Norman Koch <norman.koch@tu-dresden.de>
2025-12-02 11:12:41 +0100

    moved custom_layers to debug_layer

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 		_js("tf/tf.min.js");
 		_js("base_wrappers.js");
 		_js("libs/jstat.min.js");
-		_js("custom_layers.js");
+		_js("debug_layer.js");
 		_js("snake_activation_layer.js");
 		_js("multi_activation.js");
 		_js("libs/jsmanipulate.js", 1, 1);
