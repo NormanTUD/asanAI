d3b1f04b97e4dd3f07071e51dd0591e16c5dc1e1
n <norman.koch@tu-dresden.de>
2025-12-06 14:46:19 +0100

    new equations

--- math_mode.js
+++ math_mode.js
@@ -1070,17 +1070,17 @@ function get_loss_equations() {
 		"meanAbsoluteError": "\\mathrm{MAE} = \\frac{1}{n} \\sum_{i=1}^n \\left|y_i - \\hat{y}_i\\right|",
 		"meanSquaredError": "\\mathrm{MSE} = \\frac{1}{n} \\sum_{i=1}^n \\left(y_i - \\hat{y}_i\\right)^2",
 		"rmse": "\\mathrm{RMSE} = \\sqrt{\\mathrm{MSE}} = \\sqrt{\\frac{1}{n} \\sum_{i=1}^n \\left(y_i - \\hat{y}_i\\right)^2}",
-		"categoricalCrossentropy": "\\text{Categorical Crossentropy:} -\\sum_{i=1}^n y_i \\log\\left(\\hat{y}_i\\right)",
-		"binaryCrossentropy": "\\text{Binary Crossentropy:} -\\frac{1}{n} \\sum_{i=1}^n y_i \\cdot \\log\\left(\\hat{y}_i\\right) + 1\\left(-y_i\\right) \\cdot \\log\\left(1 - \\hat{y}_i\\right)",
-		"meanSquaredLogarithmicError": "\\text{Mean Squared Logarithmic Error:} \\frac{1}{n} \\sum_{i=0}^n \\left(log\\left(y_i + 1\\right)- \\log\\left(\\hat{y}_i + 1\\right)\\right)^2",
-		"poisson": "\\text{Poisson:} \\frac{1}{n} \\sum_{i=0}^n \\left(\\hat{x}_i - y_i\\cdot \\log\\left(\\hat{y}_i\\right)\\right)",
-		"squaredHinge": "\\text{Squared Hinge:} \\sum_{i=0}^n \\left(\\mathrm{max}\\left(0, 1 - y_i \\cdot \\hat{y}_i\\right)^ 2\\right)",
-		"logcosh": "\\text{logcosh:} \\sum_{i=0}^n \\log(\\cosh\\left(\\hat{y}_i - y_i\\right))",
-		"meanAbsolutePercentageError": "\\text{MAPE} = \\frac{1}{n} \\sum_{t=1}^{n} \\left|\\frac{\\hat{y} - y}{\\hat{y}}\\right|",
+		"categoricalCrossentropy": "\\text{Categorical Crossentropy:} -\\frac{1}{n} \\sum_{i=1}^n \\sum_{j=1}^m y_{i,j} \\log\\left(\\hat{y}_{i,j}\\right)",
+		"binaryCrossentropy": "\\text{Binary Crossentropy:} -\\frac{1}{n} \\sum_{i=1}^n \\left(y_i \\log\\left(\\hat{y}_i\\right) + (1 - y_i) \\log\\left(1 - \\hat{y}_i\\right)\\right)",
+		"meanSquaredLogarithmicError": "\\text{Mean Squared Logarithmic Error:} \\frac{1}{n} \\sum_{i=1}^n \\left(\\log\\left(y_i + 1\\right)- \\log\\left(\\hat{y}_i + 1\\right)\\right)^2",
+		"poisson": "\\text{Poisson:} \\frac{1}{n} \\sum_{i=1}^n \\left(\\hat{y}_i - y_i \\log\\left(\\hat{y}_i\\right)\\right)",
+		"squaredHinge": "\\text{Squared Hinge:} \\frac{1}{n} \\sum_{i=1}^n \\left(\\max\\left(0, 1 - y_i \\cdot \\hat{y}_i\\right)\\right)^2",
+		"logcosh": "\\text{logcosh:} \\frac{1}{n} \\sum_{i=1}^n \\log\\left(\\cosh\\left(\\hat{y}_i - y_i\\right)\\right)",
+		"meanAbsolutePercentageError": "\\text{MAPE} = \\frac{1}{n} \\sum_{i=1}^{n} \\left|\\frac{y_i - \\hat{y}_i}{y_i}\\right|",
 		"categoricalHinge": "\\text{Categorical Hinge:} \\frac{1}{n} \\sum_{i=1}^n \\max\\left(0, \\max_{j \\neq t_i}(\\hat{y}_{i,j}) - \\hat{y}_{i,t_i} + 1\\right)",
 		"hinge": "\\text{Hinge:} \\frac{1}{n} \\sum_{i=1}^n \\max\\left(0, 1 - y_i \\cdot \\hat{y}_i\\right)",
 		"sparseCategoricalCrossentropy": "\\text{Sparse Categorical Crossentropy:} -\\frac{1}{n} \\sum_{i=1}^n \\log\\left(\\hat{y}_{i,t_i}\\right)",
-		"kullbackLeiblerDivergence": "\\text{KL Divergence:} \\sum_{i=1}^n y_i \\cdot \\log\\left(\\frac{y_i}{\\hat{y}_i}\\right)"
+		"kullbackLeiblerDivergence": "\\text{Kullback-Leibler-Divergence:} \\sum_{i=1}^n y_i \\cdot \\log\\left(\\frac{y_i}{\\hat{y}_i}\\right)"
 	};
 }
 
