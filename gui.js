e7df1575232d3bccbd9550c2cff70ea54c1aad65
Norman Koch <norman.koch@tu-dresden.de>
2025-12-16 13:12:24 +0100

    fix

--- gui.js
+++ gui.js
@@ -2767,7 +2767,7 @@ async function initializer_layer_options(thisitem) {
 
 	await updated_page(null, 1);
 
-	show_visual_explanations();
+	await show_visual_explanations();
 }
 
 async function set_option_for_layer_by_layer_nr(layer_idx) {
