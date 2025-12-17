9687ce175a784eb689160371d3e7a2d8788a3385
n <norman.koch@tu-dresden.de>
2025-12-17 08:02:29 +0100

    fix

--- gui.js
+++ gui.js
@@ -2981,7 +2981,7 @@ function option_for_layer(nr) {
 
 	var str = "";
 	str += "<tr>";
-	str += "<td style='width: 150px'>";
+	str += "<td style='width: 160px'>";
 	str += "<button style='cursor: pointer' class='show_data layer_options_button' onclick='toggle_layer_options(this)'><img src='_gui/icons/settings.svg' class='icon_small' />&nbsp;<span class='TRANSLATEME_settings'></span></button>";
 	str += "<button style='cursor: pointer' class='show_data layer_options_button' onclick='toggle_layer_help(this)'><img src='_gui/icons/bulb.svg' class='icon_small' /></button>";
 	str += "</td>";
