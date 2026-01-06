6b5e7ae069cef6cea81e3c7faa326d83c78f9878
Norman Koch <norman.koch@tu-dresden.de>
2026-01-06 10:19:05 +0100

    fix

--- blog/init.js
+++ blog/init.js
@@ -1,5 +1,5 @@
 window.onresize = () => {
-	['lin-loss-chart', 'lin-data-chart', 'gauss-chart', 'deep-loss-chart', 'deep-data-chart'].forEach(id => {
+	['lin-loss-chart', 'lin-data-chart', 'deep-loss-chart', 'deep-data-chart'].forEach(id => {
 		const el = document.getElementById(id);
 		if(el) Plotly.Plots.resize(el);
 	});
@@ -10,11 +10,6 @@ window.onload = () => {
 	initBlock('lin'); initBlock('deep'); initVisionLab();
 
 	const x=[], y=[]; for(let i=-4; i<=4; i+=0.1) { x.push(i); y.push(Math.exp(-0.5*i*i)/Math.sqrt(2*Math.PI)); }
-	Plotly.newPlot('gauss-chart', [{x,y,fill:'tozeroy', line:{color:'#3b82f6'}}], {
-		margin:{t:30, b:30, l:40, r:10}, 
-		title: 'Standardnormalverteilung',
-		autosize: true
-	}, {responsive: true});
 
 	renderMarkdown();
 	if (window.MathJax) MathJax.typeset();
