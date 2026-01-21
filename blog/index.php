95a5b97a5b770ee03eb77b261846ac2b706e0d28
Norman Koch <norman.koch@tu-dresden.de>
2026-01-21 15:16:52 +0100

    fix

--- blog/index.php
+++ blog/index.php
@@ -1,6 +1,6 @@
 <?php include_once("functions.php"); ?>
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
