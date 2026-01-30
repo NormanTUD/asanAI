ec9f60b6b067c57bb6010e5d417183c282d86244
n <norman.koch@tu-dresden.de>
2026-01-30 13:19:49 +0100

    fix

--- blog/literature.js
+++ blog/literature.js
@@ -61,6 +61,7 @@ window.bibData = {
 		"author": "Wikipedia contributors",
 		"year": "2026",
 		"title": "Goodhart's law - Wikipedia, The Free Encyclopedia",
+		"alternativetitle": "Goodhart's Law",
 		"url": "https://en.wikipedia.org/w/index.php?title=Goodhart%27s_law&oldid=1332895881",
 	},
 	"mooreslaw": {
