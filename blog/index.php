10f48eeff216ab41072914a3a7b453b16fababc3
n <norman.koch@tu-dresden.de>
2026-01-31 10:01:49 +0100

    order

--- blog/index.php
+++ blog/index.php
@@ -18,11 +18,11 @@
 <?php
 	incl("From $ 1 + 1 = 2 $ to ChatGPT: Basic math concepts", "intro");
 	incl("Brief History of AI", "history");
+	incl("Statistical Foundations: The Language of AI", "statistics");
 	incl("Loss: Teaching through Failure", "losslab");
 	incl('Derivatives: How AI "Learns" to get better', "derivativelab");
 	incl("Optimizer", "optimizerlab");
 	incl("Smallest possible neural network", "minimalneuron");
-	incl("Statistical Foundations: The Language of AI", "statistics");
 	incl("Activation Functions: The Neural Decision Makers", "activationlab");
 	incl("See the training of a Neural Network in action", "traininglab");
 	incl("Computer Vision", "visionlab");
