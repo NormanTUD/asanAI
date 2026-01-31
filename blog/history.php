8388c3f3f8cc34bb6431fbdf6f3fe8a6b29ef14c
n <norman.koch@tu-dresden.de>
2026-01-31 22:19:28 +0100

    fix

--- blog/history.php
+++ blog/history.php
@@ -33,7 +33,7 @@ $$
 
 By proving that truth could be derived through the mechanical application of formal rules, Aristotle provided the blueprint for everything from medieval scholasticism to modern computation.
 
-This transition from philosophical dialogue to a rigid logical calculus paved the way for thinkers to treat thought as a form of algebra, leading directly to the work of **Gottfried Wilhelm Leibniz**.
+This transition from philosophical dialogue to a rigid logical calculus paved the way for thinkers to treat thought as a form of algebra, leading directly to the work of \citeauthor{arsmagna}.
 
 ## The idea that logical thought can be mechanically calculated
 
