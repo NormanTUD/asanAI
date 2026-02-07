73e97efaa873d864c198ca2f357a5f9dcae76376
n <norman.koch@tu-dresden.de>
2026-02-07 15:27:27 +0100

    fix

--- blog/overandunderfittinglab.php
+++ blog/overandunderfittinglab.php
@@ -11,14 +11,13 @@ In this interactive lab, we are tasking an Artificial Intelligence (a neural net
 
 ## The Math: How the Model "Thinks"
 
-The AI in this lab uses a **Polynomial Regression** model. Instead of "knowing" it's a sine wave, it tries to build an equation by adding together different powers of $x$.
 
-### The General Formula
 Here, we build a function $f(x)$ that looks like this:
 
 $$f(x) = w_n x^n + w_{n-1} x^{n-1} + \dots + w_1 x^1 + b = \sum_{i=1}^{n} w_i x^i + b$$
 
-Where:
+This is a **Polynomial Regression** model. Instead of "knowing" it's a sine wave, it tries to build an equation by adding together different powers of $x$, Where:
+
 * **$n$ (Polynomial Degree):** This is the "Complexity" slider.
     * A **Degree 1** model is just a straight line ($w_1 x + b$).
     * A **Degree 10** model is a complex curve that can bend up to 9 times.
