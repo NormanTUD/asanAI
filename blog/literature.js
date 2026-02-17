6d8692bf3f72c461f25a83cc029ae8430b203bdf
n <norman.koch@tu-dresden.de>
2026-02-17 09:06:57 +0100

    fix

--- blog/literature.js
+++ blog/literature.js
@@ -218,7 +218,8 @@ window.bibData = {
 		url: "https://arxiv.org/abs/2203.02155",
 		title: "Training language models to follow instructions with human feedback",
 		author: "Long Ouyang, Jeff Wu, Xu Jiang, Diogo Almeida, Carroll L. Wainwright, Pamela Mishkin, Chong Zhang, Sandhini Agarwal, Katarina Slama, Alex Ray, John Schulman, Jacob Hilton, Fraser Kelton, Luke Miller, Maddie Simens, Amanda Askell, Peter Welinder, Paul Christiano, Jan Leike, Ryan Lowe",
-		year: 2022
+		year: 2022,
+		alternativetitle: "InstructGPT"
 	},
 	"bahdanau2014": {
 		url: "https://arxiv.org/abs/1409.0473",
