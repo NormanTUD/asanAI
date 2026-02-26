8435bb1e78398dcb740d762bb20f7c7e003916c4
n <norman.koch@tu-dresden.de>
2026-02-26 19:37:48 +0100

    fix

--- blog/intuition.php
+++ blog/intuition.php
@@ -6,10 +6,10 @@
 We can think of LLMs as machines that predict the next most likely word, given a context. Only one word at a time. That word is appended to the input and fed back in, repeating until a special `|endoftext|` token signals the end. In this example, the user enters the text "Once upon a" and lets the LLM continue.
 
 **Step 1: The First Guess**
-$$\text{Next Word} = \text{LLM}(\text{"Once upon a"}) \rightarrow \text{"time"}$$
+$$\text{Next Word} = \text{LLM}(\underbrace{\text{"Once upon a"}}_{\text{User Input}}) \rightarrow \text{"time"}$$
 
 **Step 2: The Glue**
-$$\text{New Input} = \text{"Once upon a"} + \text{"time"} = \text{"Once upon a time"}$$
+$$\text{New Input} = \underbrace{\text{"Once upon a"}}_{\text{User Input}} + \text{"time"} = \text{"Once upon a time"}$$
 
 **Step 3: The Repeat**
 $$\text{Next Word} = \text{LLM}(\text{"Once upon a time"}) \rightarrow \text{"there"}$$
