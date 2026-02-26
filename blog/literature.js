e35189697f95fa7f31adfcd3410adc7b8afc4be2
n <norman.koch@tu-dresden.de>
2026-02-26 23:10:11 +0100

    fix

--- blog/literature.js
+++ blog/literature.js
@@ -298,7 +298,7 @@ window.bibData = {
 		author: "Matthew E. Peters, Mark Neumann, Mohit Iyyer, Matt Gardner, Christopher Clark, Kenton Lee, Luke Zettlemoyer",
 		url: "https://aclanthology.org/N18-1202/",
 		year: 2018,
-		alternative: "ELMo"
+		alternativetitle: "ELMo"
 	},
 	"oevresdeformat": {
 		url: "https://archive.org/details/oeuvresdefermat02ferm",
