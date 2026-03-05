fc33819c2fc005e1385c593019f655570be05c19
n <norman.koch@tu-dresden.de>
2026-03-05 12:44:05 +0100

    fix

--- blog/php_syntax_check.sh
+++ blog/php_syntax_check.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # Recursively check for syntax errors
 find . -type f -name "*.php" -exec php -l {} \; | grep -v "No syntax errors detected"
 
@@ -10,4 +8,4 @@ if [ ${PIPESTATUS[0]} -ne 0 ]; then
     exit 1
 fi
 
-find . -name "*.php" -print0 | xargs -0 -n1 php -l
+exit 0
