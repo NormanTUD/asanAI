691407aeaaf32ef42f4dbcbd3abaa705763d517b
n <norman.koch@tu-dresden.de>
2026-03-06 15:55:13 +0100

    fix

--- blog/index.php
+++ blog/index.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>From 1 + 1 = 2 to ChatGPT</title>
-    <?php load_base_js(); ?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>From 1 + 1 = 2 to ChatGPT</title>
+	<?php load_base_js(); ?>
 </head>
 
 <div id="loader">
