b5d8ee41270efe1cfcd59d9da25ec62fc5b4c77c
Norman Koch <norman.koch@tu-dresden.de>
2026-03-30 10:05:40 +0200

    fix

--- blog/literature.js
+++ blog/literature.js
@@ -2,7 +2,7 @@ window.bibData = {
 	"notesonlight": {
 		url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Notes_of_a_course_of_nine_lectures_on_light_delivered_at_The_Royal_Institution_of_Great_Britain%2C_April_8-June_3%2C_1869_%28IA_b28129994%29.pdf",
 		title: "Notes on Light",
-		year: 1891,
+		year: 1872,
 		author: "John Tyndall"
 	},
 	"typewriterpatent": {
