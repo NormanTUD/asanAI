dcfe4e91aeff09bfefc505453c78103b54526656
Norman Koch <norman.koch@tu-dresden.de>
2026-04-07 14:25:08 +0200

    fix

--- blog/math.php
+++ blog/math.php
@@ -293,7 +293,7 @@ The idea of summing infinite series to achieve finite results dates back to anti
 
 In early 1671, James Gregory independently discovered something like the general Maclaurin series (see \citetitle{gregoryachievements}), but never published his method, believing he had merely rediscovered work by Isaac Newton. In 1691–1692, Newton wrote down an explicit general formulation in an unpublished draft of \citetitle{newtonquadratura}, but the relevant sections were omitted from the 1704 publication.
 
-It was not until 1715 that \citeauthor{taylor1715} published the first general method for constructing these series (pp. 21–23), after whom they are now named. The special case centered at zero was later named after \citeauthor{maclaurin}, who published it in the mid-18th century.
+It was not until 1715 that \citeauthor{taylor1715} published the first general method for constructing these series (pp. 21–23), after whom they are now named. The special case centered at zero was later named after \citeauthor{maclaurin}, who published it in the 18th century.
 </div>
 
 ## Exponentiation
