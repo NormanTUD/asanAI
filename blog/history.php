250127d6b692bc301035364755f4707df108b3f9
n <norman.koch@tu-dresden.de>
2026-04-13 07:41:26 +0200

    fix

--- blog/history.php
+++ blog/history.php
@@ -364,7 +364,7 @@ The term "Artificial Intelligence" was coined in \citeyear{darthmouthartificiali
 </figure>
 
 <div class="smart-quote" data-cite="hebb1949organizationofbehaviour" data-page=62>
-<div class="full-quote">When an axon of cell A is near enough to excite a cell B and repeatedly or persistently takes part in firing it... some growth process or metabolic change takes place in one or both cells such that A's efficiency, as one of the cells firing B, is increased.</div>
+<div class="full-quote">When an axon of cell A is near enough to excite a cell B and repeatedly or persistently takes part in firing it [...] some growth process or metabolic change takes place in one or both cells such that A's efficiency, as one of the cells firing B, is increased.</div>
 <div class="short-quote">What wires together, fires together</div>
 </div>
 
