1dbc26abf36629db0e45a3bcd9dd0e81583e71c5
n <norman.koch@tu-dresden.de>
2026-05-21 19:21:06 +0200

    fix

--- blog/algorithms.php
+++ blog/algorithms.php
@@ -1,7 +1,7 @@
 <?php include_once("functions.php"); ?>
 
 <div class="md">
-When we train a neural network on a simple task like modular addition, something remarkable happens. The network doesn't just memorize the answers, it *discovers* an algorithm. Specifically, it learns to use the **Discrete Fourier Transform** to convert addition into wave interference \cite[Nanda et al., 2023]{nanda2023grokking}.
+When we train a neural network on a simple task like modular addition, something remarkable happens. The network doesn't just memorize the answers, it *discovers* an algorithm. Specifically, it learns to use the **Discrete Fourier Transform** to convert addition into wave interference, as described by \cite[Nanda et al., 2023]{nanda2023grokking}.
 
 This section walks through, step by step, exactly *how* a tiny one-layer Transformer computes:
 
