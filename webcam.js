e0aed1f554c208013ed440f62abf75daf4a0fbc6
n <norman.koch@tu-dresden.de>
2026-06-09 16:53:15 +0200

    fix

--- webcam.js
+++ webcam.js
@@ -753,7 +753,7 @@ function alter_text_webcam_series () {
 	};
 
 	var s = "&#128248; x " + number;
-	s = s + " (" + formatNumber(nr_of_imgs_per_second) + "/s)";
+	s = s + " (" + nr_of_imgs_per_second + "/s)";
 
 	$(".webcam_series_button").html(s);
 }
