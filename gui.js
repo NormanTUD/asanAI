d75b04248e4f38eb899dd4d69fb15d691992b77f
n <norman.koch@tu-dresden.de>
2026-06-19 07:54:54 +0200

    new css

--- gui.js
+++ gui.js
@@ -2912,8 +2912,8 @@ function option_for_layer(nr) {
 	var str = "";
 	str += "<tr>";
 	str += "<td style='width: 110%;'>";
-	str += "<button style='cursor: pointer' class='show_data layer_options_button' onclick='toggle_layer_options(this)'><img src='_gui/icons/settings.svg' class='icon_small' />&nbsp;<span class='TRANSLATEME_settings'></span></button>";
-	str += "<button style='cursor: pointer' class='show_data layer_help_button' onclick='toggle_layer_help(this)'>?</button>";
+	str += "<button style='cursor: pointer' class='left_border_radius show_data layer_options_button' onclick='toggle_layer_options(this)'><img src='_gui/icons/settings.svg' class='icon_small' />&nbsp;<span class='TRANSLATEME_settings'></span></button>";
+	str += "<button style='cursor: pointer' class='right_border_radius show_data layer_help_button' onclick='toggle_layer_help(this)'>?</button>";
 	str += "</td>";
 	str += "<td>";
 	str += `<select id="${option_for_layer_id}" onfocus='disable_invalid_layers_event(event, this)' onchange='${this_event}' class='input_data layer_type'>`;
