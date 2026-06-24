70ec810e1dbcbab7477ba36294ebbae51c166967
Norman Koch <norman.koch@tu-dresden.de>
2026-06-24 13:14:45 +0200

    fix

--- blog/lndw/pe_orbit_viz.js
+++ blog/lndw/pe_orbit_viz.js
@@ -19,7 +19,7 @@ const PEOrbitViz = (() => {
         { word: "schläft", pos: [0.35, -0.55], color: '#94a3b8' },
         { word: "die", pos: [-0.6, 0.9], color: '#94a3b8' },
         { word: "auf", pos: [-0.6, 0.1], color: '#94a3b8' },
-        { word: "Sofa", pos: [1.3, -0.3], color: '#94a3b8' },
+        { word: "Sofa", pos: [1.6, -0.4], color: '#94a3b8' },
     ];
 
     const POS_COLORS = [
