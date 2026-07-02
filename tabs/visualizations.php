186fd660da37b836928c847fd2daad371b22cbd3
n <norman.koch@tu-dresden.de>
2026-07-02 08:18:00 +0200

    order

--- tabs/visualizations.php
+++ tabs/visualizations.php
@@ -17,45 +17,7 @@
 		<div id="fcnn"><canvas style='width: 100%' id="fcnn_canvas" /></div>
 	</div>
 
-	<div id="dimensionality_river" class="tab">
-	</div>
-
-	<div id="activation_atlas" class="tab">
-	</div>
-
-	<div id="health_status" class="tab">
-		<div class="TRANSLATEME_no_health_status_data"></div>
-	</div>
-
-	<div id="weight_analysis" class="tab">
-		<div class="TRANSLATEME_no_weight_analysis"></div>
-	</div>
-
-
-	<div id="activation_plot_tab" class="tab">
-		<span id="activation_plot_name" style="display: none"></span>
-		<div id="activation_plot" style="display: none"></div>
-	</div>
-
-	<div id="maximally_activated" class="tab maximally_activated_class">
-		<div>
-			<span class="temml_me">x = </span> <span class="TRANSLATEME_input_image"></span><br>
-			<span class="temml_me">x* = </span> <span class="TRANSLATEME_generated_image"></span><br>
-			<span class="temml_me">W = </span> <span class="TRANSLATEME_weights"></span><br>
-			<span class="temml_me">b = </span> <span class="TRANSLATEME_bias"></span><br>
-			<span class="temml_me">f(\textbf{x}; \textbf{W}, b) = </span> <span class="TRANSLATEME_activation_function_of_neuron"><br>
-			<span class="temml_me">\textbf{x}* = \mathrm{argmax}_xf(\textbf{x}; \textbf{W}, b)</span> <br>
-		</div>
-		<span class="TRANSLATEME_maximally_activated_explanation"></span>
-		<br>
-		<button onclick="smaller_maximally_activated_neurons()"><span class="TRANSLATEME_smaller"></span></button>
-		<button onclick="larger_maximally_activated_neurons()"><span class="TRANSLATEME_larger"></span></button>
-		<button onclick="reset_maximally_activated_neurons()"><span class="TRANSLATEME_reset"></span></button>
-		<button onclick="delete_maximally_activated_predictions()"><span class="TRANSLATEME_delete_predictions"></span></button>
-		<div id="maximally_activated_content"></div>
-	</div>
-
-	<div id="math_tab" class="tab" class="tab">
+	<div id="math_tab" class="tab">
 		<table>
 			<tr>
 				<td class="TRANSLATEME_no_decimal_points_math_mode"></td>
@@ -80,11 +42,48 @@
 		</div>
 	</div>
 
-	<div id="weight_surfaces" class="tab" class="tab">
+	<div id="maximally_activated" class="tab maximally_activated_class">
+		<div>
+			<span class="temml_me">x = </span> <span class="TRANSLATEME_input_image"></span><br>
+			<span class="temml_me">x* = </span> <span class="TRANSLATEME_generated_image"></span><br>
+			<span class="temml_me">W = </span> <span class="TRANSLATEME_weights"></span><br>
+			<span class="temml_me">b = </span> <span class="TRANSLATEME_bias"></span><br>
+			<span class="temml_me">f(\textbf{x}; \textbf{W}, b) = </span> <span class="TRANSLATEME_activation_function_of_neuron"><br>
+			<span class="temml_me">\textbf{x}* = \mathrm{argmax}_xf(\textbf{x}; \textbf{W}, b)</span> <br>
+		</div>
+		<span class="TRANSLATEME_maximally_activated_explanation"></span>
+		<br>
+		<button onclick="smaller_maximally_activated_neurons()"><span class="TRANSLATEME_smaller"></span></button>
+		<button onclick="larger_maximally_activated_neurons()"><span class="TRANSLATEME_larger"></span></button>
+		<button onclick="reset_maximally_activated_neurons()"><span class="TRANSLATEME_reset"></span></button>
+		<button onclick="delete_maximally_activated_predictions()"><span class="TRANSLATEME_delete_predictions"></span></button>
+		<div id="maximally_activated_content"></div>
+	</div>
+
+	<div id="activation_plot_tab" class="tab">
+		<span id="activation_plot_name" style="display: none"></span>
+		<div id="activation_plot" style="display: none"></div>
+	</div>
+
+	<div id="weight_surfaces" class="tab">
 		<div id="weight_surfaces_content"></div>
 	</div>
 
-	<div id="loss_landscape_tab" class="tab" class="tab">
+	<div id="health_status" class="tab">
+		<div class="TRANSLATEME_no_health_status_data"></div>
+	</div>
+
+	<div id="dimensionality_river" class="tab">
+	</div>
+
+	<div id="activation_atlas" class="tab">
+	</div>
+
+	<div id="weight_analysis" class="tab">
+		<div class="TRANSLATEME_no_weight_analysis"></div>
+	</div>
+
+	<div id="loss_landscape_tab" class="tab">
 		<table>
 			<tbody>
 
@@ -159,6 +158,6 @@
 		<div id="loss_landscape"></div>
 	</div>
 
-	<div id="layer_input_groups" class="tab" class="tab">
+	<div id="layer_input_groups" class="tab">
 	</div>
 </div>
