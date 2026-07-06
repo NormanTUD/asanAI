a596cde25ca58c7a8f68e34ffe23d925f9cf3335
n <norman.koch@tu-dresden.de>
2026-07-06 08:47:02 +0200

    updated_page

--- index.php
+++ index.php
@@ -119,6 +119,7 @@
 		_js("input_shape.js");
 		_js("clipboard_and_download.js");
 		_js("drawing.js");
+		_js("updated_page.js");
 		_js("gui.js");
 		_js("summary.js");
 		_js("custom_images.js");
