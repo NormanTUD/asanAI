c24704404f80afa47555efe6e0c149c92e288bc2
n <norman.koch@tu-dresden.de>
2026-07-06 08:20:45 +0200

    moved stuff to skip_connection.js

--- layers_gui.js
+++ layers_gui.js
@@ -127,19 +127,6 @@ function build_eval_option_html(item, type, nr) {
 	return "";
 }
 
-
-// ─────────────────────────────────────────────────────────────────────────────
-// build_skip_connection_html
-// Appends skip connection option if applicable
-// ─────────────────────────────────────────────────────────────────────────────
-
-function build_skip_connection_html(type, nr) {
-	if (skip_connection_excluded_types.includes(type)) {
-		return "";
-	}
-	return add_skip_connection_option(type, nr);
-}
-
 // ─────────────────────────────────────────────────────────────────────────────
 // collapse_advanced_section
 // Animates the advanced section closed and rotates the arrow icon
@@ -226,14 +213,14 @@ async function show_layers(number) {
 		layers_container_str +=
 			"<li class='ui-sortable-handle'><span class='layer_start_marker'></span><div class='container layer layer_setting glass_box'>" +
 			"<div style='display:none' class='warning_container'><span style='color: yellow'>&#9888;</span><span class='warning_layer'></span></div>" +
-				remove +
-				add +
-				"<span class='layer_nr_desc'></span>" +
-				"<span class='layer_identifier'></span>" +
-				"<table class='configtable'>" +
-					option_for_layer(layer_idx) +
-				"</table>" +
-				"</div>" +
+			remove +
+			add +
+			"<span class='layer_nr_desc'></span>" +
+			"<span class='layer_identifier'></span>" +
+			"<table class='configtable'>" +
+			option_for_layer(layer_idx) +
+			"</table>" +
+			"</div>" +
 			"<span class='layer_end_marker'></span>" +
 			"</li>"
 		;
@@ -452,7 +439,6 @@ function insert_activation_option_trs(layer_nr, option_type) {
 	// (no need to hide them behind advanced toggle)
 }
 
-
 async function insert_activation_options(layer_nr) {
 	// TODO NOT YET USED
 	assert(typeof(layer_nr) == "number", "layer_nr must be of the type of number but is: " + typeof(layer_nr));
@@ -600,127 +586,6 @@ function add_number_lstm_cells_option(type, nr) {
 	var res = get_tr_str_for_layer_table("LSTM Cells", "number_lstm_cells", "number", data, nr, null, null);
 
 	return res;
-}
-
-function add_skip_connection_option(type, nr) {
-	if (skip_connection_excluded_types.includes(type)) {
-		return "";
-	}
-
-	var checked_attr = "";
-	var current_initializer = "glorotUniform";
-
-	/*
-	if (skip_connection_settings[nr] && skip_connection_settings[nr].enabled === true) {
-		checked_attr = " checked ";
-	}
-	*/
-	if (skip_connection_settings[nr] && skip_connection_settings[nr].initializer) {
-		current_initializer = skip_connection_settings[nr].initializer;
-	}
-
-	var str = "";
-	str += "<tr class='skip_connection_tr'>";
-	str += "<td><span class='TRANSLATEME_skip_connection'>Skip Connection</span>:</td>";
-	str += "<td>";
-	str += "<input type='checkbox' class='skip_connection_enabled' " + checked_attr + " onchange='toggle_skip_connection(find_layer_number_by_element(this), this)' />";
-	str += "</td>";
-	str += "</tr>";
-
-	// Initializer selector row (hidden when skip is disabled)
-	var init_display = checked_attr ? "" : "display:none;";
-	str += "<tr class='skip_connection_initializer_tr' style='" + init_display + "'>";
-	str += "<td>Skip Initializer:</td>";
-	str += "<td>";
-	str += "<select class='input_field skip_connection_initializer_select' onchange='update_skip_connection_initializer(find_layer_number_by_element(this), this); insert_skip_initializer_options(find_layer_number_by_element(this), this); updated_page(null, null, this);'>";
-	for (var key in initializers) {
-		if (initializers.hasOwnProperty(key)) {
-			var selected_attr = (key === current_initializer) ? " selected" : "";
-			str += "<option value='" + key + "'" + selected_attr + ">" + initializers[key] + "</option>";
-		}
-	}
-	str += "</select>";
-	str += "</td>";
-	str += "</tr>";
-
-	// Sub-options for the skip initializer (seed, mean, stddev, etc.)
-	// NOTE: We pass NO section_class here because build_advanced_section will
-	// add the class to ALL <tr> elements in the advanced_str (including these).
-	// The section_class is applied by build_advanced_section after this function returns.
-	if (checked_attr) {
-		// Pass null for section_class - build_advanced_section handles it
-		str += _get_skip_initializer_sub_options_html(nr, current_initializer, null);
-	}
-
-	return str;
-}
-
-function _get_skip_initializer_sub_options_html(nr, initializer_name, section_class) {
-    if (!initializer_name || !initializer_options[initializer_name]) {
-        return "";
-    }
-
-    var options = initializer_options[initializer_name]["options"];
-    if (!options || options.length === 0) {
-        return "";
-    }
-
-    var str = "";
-    var current_params = (skip_connection_settings[nr] && skip_connection_settings[nr].initializer_params) || {};
-
-    // Determine display style: hidden if section_class is provided and section is collapsed
-    var display_style = "";
-    if (section_class) {
-        display_style = "display: none;";
-    }
-
-    for (var i = 0; i < options.length; i++) {
-        var opt_name = options[i];
-        var opt_value = (opt_name in current_params) ? current_params[opt_name] : _get_skip_initializer_default_value(opt_name, nr);
-
-        var class_attr = "skip_connection_initializer_option_tr";
-        if (section_class) {
-            class_attr += " " + section_class;
-        }
-
-        str += "<tr class='" + class_attr + "' style='" + display_style + "'>";
-        str += "<td>" + opt_name.charAt(0).toUpperCase() + opt_name.slice(1) + ":</td>";
-        str += "<td>";
-        if (opt_name === "mode") {
-            str += "<select class='input_field skip_connection_initializer_param_" + opt_name + "' onchange='update_skip_connection_initializer_param(find_layer_number_by_element(this), \"" + opt_name + "\", this); updated_page(null, null, this);'>";
-            for (var mk in mode_modes) {
-                var sel = (mk === ("" + opt_value)) ? " selected" : "";
-                str += "<option value='" + mk + "'" + sel + ">" + mode_modes[mk] + "</option>";
-            }
-            str += "</select>";
-        } else if (opt_name === "distribution") {
-            str += "<select class='input_field skip_connection_initializer_param_" + opt_name + "' onchange='update_skip_connection_initializer_param(find_layer_number_by_element(this), \"" + opt_name + "\", this); updated_page(null, null, this);'>";
-            for (var dk in distribution_modes) {
-                var sel2 = (dk === ("" + opt_value)) ? " selected" : "";
-                str += "<option value='" + dk + "'" + sel2 + ">" + distribution_modes[dk] + "</option>";
-            }
-            str += "</select>";
-        } else {
-            str += "<input class='input_field skip_connection_initializer_param_" + opt_name + "' type='number' value='" + opt_value + "' min='-3.4e+38' max='3.4e+38' onchange='update_skip_connection_initializer_param(find_layer_number_by_element(this), \"" + opt_name + "\", this); updated_page(null, null, this);' />";
-        }
-        str += "</td>";
-        str += "</tr>";
-    }
-
-    return str;
-}
-
-function _get_skip_initializer_default_value(opt_name, nr) {
-    if (opt_name === "seed") return get_unique_seed_for_layer(nr);
-    if (opt_name === "mean") return 0;
-    if (opt_name === "stddev") return 0.05;
-    if (opt_name === "value") return 1;
-    if (opt_name === "minval") return -0.05;
-    if (opt_name === "maxval") return 0.05;
-    if (opt_name === "scale") return 1;
-    if (opt_name === "mode") return "fanIn";
-    if (opt_name === "distribution") return "normal";
-    return 1;
 }
 
 async function add_layer(item) {
@@ -1909,37 +1774,6 @@ function rename_tmp_onchange() {
 	enforce_all_advanced_section_visibility();
 }
 
-function get_skip_connection_info(layer_nr) {
-    if (!skip_connection_settings[layer_nr]) {
-        return { enabled: false, strength: 1.0 };
-    }
-
-    var strength = _compute_skip_strength_from_model(layer_nr);
-
-    return {
-        enabled: skip_connection_settings[layer_nr].enabled,
-        strength: strength
-    };
-}
-
-function update_skip_connection_strength(layer_nr, elem) {
-	var val = parseFloat($(elem).val());
-
-	if (!skip_connection_settings[layer_nr]) {
-		skip_connection_settings[layer_nr] = { enabled: false, strength: val };
-	}
-
-	skip_connection_settings[layer_nr].strength = val;
-	$("#skip_conn_strength_val_" + layer_nr).text(val.toFixed(2));
-
-	updated_page(); // await not possible here
-}
-
-function update_skip_connection_strength_display(layer_nr, elem) {
-	var val = parseFloat($(elem).val());
-	$("#skip_conn_strength_val_" + layer_nr).text(val.toFixed(2));
-}
-
 // ─────────────────────────────────────────────────────────────────────────────
 // _get_section_class_from_row
 // Extracts the advanced_options_section_* class from a given row element.
@@ -1947,17 +1781,128 @@ function update_skip_connection_strength_display(layer_nr, elem) {
 // ─────────────────────────────────────────────────────────────────────────────
 
 function _get_section_class_from_row($row) {
-    if (!$row || !$row.length) {
-        return null;
-    }
+	if (!$row || !$row.length) {
+		return null;
+	}
 
-    var row_classes = ($row.attr("class") || "").split(/\s+/);
+	var row_classes = ($row.attr("class") || "").split(/\s+/);
 
-    for (var i = 0; i < row_classes.length; i++) {
-        if (row_classes[i].startsWith("advanced_options_section_")) {
-            return row_classes[i];
-        }
-    }
+	for (var i = 0; i < row_classes.length; i++) {
+		if (row_classes[i].startsWith("advanced_options_section_")) {
+			return row_classes[i];
+		}
+	}
 
-    return null;
+	return null;
+}
+
+function apply_skip_connection(input_tensor, layer_output, strength, layer_type, layer_idx) {
+	var input_shape = input_tensor.shape; // [null, ...]
+	var output_shape = layer_output.shape; // [null, ...]
+
+	// Remove batch dimension for comparison
+	var in_shape = input_shape.slice(1);
+	var out_shape = output_shape.slice(1);
+
+	var projected_input;
+
+	// Get the initializer config from skip connection settings (if set by user)
+	var skip_initializer_config = _get_skip_initializer_config(layer_idx);
+
+	// Check if shapes already match
+	if (JSON.stringify(in_shape) === JSON.stringify(out_shape)) {
+		// Shapes match, direct addition
+		projected_input = input_tensor;
+	} else if (in_shape.length === out_shape.length) {
+		// Same rank but different dimensions - need projection
+		if (in_shape.length === 1) {
+			// Dense-like: project with a Dense layer
+			var dense_config = {
+				units: out_shape[0],
+				useBias: false,
+				trainable: true,
+				name: "skip_proj_dense_" + layer_idx + "_" + uuidv4().substring(0, 8)
+			};
+			if (skip_initializer_config) {
+				dense_config.kernelInitializer = skip_initializer_config;
+			}
+			var projection = tf.layers.dense(dense_config);
+			projected_input = projection.apply(input_tensor);
+		} else if (in_shape.length === 3) {
+			// Conv2D-like: [H, W, C] -> use 1x1 conv with strides to match spatial + channel dims
+			var target_h = out_shape[0];
+			var target_w = out_shape[1];
+			var target_c = out_shape[2];
+
+			var stride_h = Math.max(1, Math.floor(in_shape[0] / target_h));
+			var stride_w = Math.max(1, Math.floor(in_shape[1] / target_w));
+
+			var conv2d_config = {
+				filters: target_c,
+				kernelSize: [1, 1],
+				strides: [stride_h, stride_w],
+				padding: "valid",
+				useBias: false,
+				trainable: true,
+				name: "skip_proj_conv2d_" + layer_idx + "_" + uuidv4().substring(0, 8)
+			};
+			if (skip_initializer_config) {
+				conv2d_config.kernelInitializer = skip_initializer_config;
+			}
+			var projection_conv = tf.layers.conv2d(conv2d_config);
+			projected_input = projection_conv.apply(input_tensor);
+
+			// After strided conv, check if spatial dims match exactly
+			var proj_shape = projected_input.shape.slice(1);
+			if (proj_shape[0] !== target_h || proj_shape[1] !== target_w) {
+				throw new Error("Spatial dimension mismatch after projection: got [" + proj_shape.join(",") + "] expected [" + out_shape.join(",") + "]");
+			}
+		} else if (in_shape.length === 2) {
+			// Conv1D-like: [steps, channels]
+			var target_steps = out_shape[0];
+			var target_channels = out_shape[1];
+
+			var stride_s = Math.max(1, Math.floor(in_shape[0] / target_steps));
+
+			var conv1d_config = {
+				filters: target_channels,
+				kernelSize: 1,
+				strides: stride_s,
+				padding: "valid",
+				useBias: false,
+				trainable: true,
+				name: "skip_proj_conv1d_" + layer_idx + "_" + uuidv4().substring(0, 8)
+			};
+			if (skip_initializer_config) {
+				conv1d_config.kernelInitializer = skip_initializer_config;
+			}
+			var projection_conv1d = tf.layers.conv1d(conv1d_config);
+			projected_input = projection_conv1d.apply(input_tensor);
+
+			var proj_shape_1d = projected_input.shape.slice(1);
+			if (proj_shape_1d[0] !== target_steps) {
+				throw new Error("Temporal dimension mismatch after 1D projection");
+			}
+		} else {
+			throw new Error("Skip connection not supported for rank " + in_shape.length);
+		}
+	} else {
+		// Different ranks - cannot apply skip connection
+		throw new Error("Cannot apply skip connection: input rank " + in_shape.length + " != output rank " + out_shape.length);
+	}
+
+	// Apply strength scaling: output = layer_output + strength * projected_input
+	if (strength === 1.0) {
+		var added = tf.layers.add({
+			name: "skip_add_" + layer_idx + "_" + uuidv4().substring(0, 8)
+		}).apply([layer_output, projected_input]);
+		return added;
+	} else {
+		// For non-unity strength, the projection weights will learn the appropriate scaling.
+		// We just do the add directly — no non-trainable scale layer.
+		var added_scaled = tf.layers.add({
+			name: "skip_add_" + layer_idx + "_" + uuidv4().substring(0, 8)
+		}).apply([layer_output, projected_input]);
+		return added_scaled;
+	}
 }
