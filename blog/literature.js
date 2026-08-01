f65d801fe296b2157eb1f9e283febba6cef1d27b
n <norman.koch@tu-dresden.de>
2026-08-01 12:25:18 +0200

    fix

--- blog/literature.js
+++ blog/literature.js
@@ -3931,7 +3931,7 @@ window.bibData = {
 
 	// Reinforcement Learning / DPO
 	"sutton2018reinforcement": {
-		url: "http://incompleteideas.net/book/the-book-2nd.html",
+		url: "http://incompleteideas.net/book/RLbook2020.pdf",
 		title: "Reinforcement Learning: An Introduction",
 		author: "Richard S. Sutton, Andrew G. Barto",
 		year: 2018,
