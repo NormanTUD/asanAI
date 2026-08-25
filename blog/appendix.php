52c7a4b0937695788cdecf30ebe38e76e18896f7
n <norman.koch@tu-dresden.de>
2026-08-25 19:52:22 +0200

    fix

--- blog/appendix.php
+++ blog/appendix.php
@@ -14,7 +14,7 @@ topics: reference, math-i, programming
 ## Training
 ### Grokking
 
-**\cite[Grokking]{grokking}** is a phenomenon in deep learning where a model suddenly transitions from **memorization** to **generalization** long after it seems to have plateaued.
+\cite[Grokking]{grokking} is a phenomenon in deep learning where a model suddenly transitions from **memorization** to **generalization** long after it seems to have plateaued.
 Originally identified by \cite[Power et al., 2022][]{power2022grokking}, this "aha moment" occurs when a model achieves 100% training accuracy but 0% validation accuracy for an extended period,
 only to have validation accuracy jump to 100% within a few epochs. This indicates a shift from high-frequency noise-fitting to the discovery of an underlying algorithmic pattern.
 Structurally, this is often marked by a transition in **attention matrices** from messy, uniform distributions to clean, highly structured representations.
