9c61698a3253328024c755542195e5a46be923e5
n <norman.koch@tu-dresden.de>
2026-08-29 10:01:04 +0200

    fix

--- blog/helper.js
+++ blog/helper.js
@@ -837,7 +837,10 @@ function bibtexify() {
 		const citedInThisBlock = new Set();
 
 		// UPDATED REGEX: Added (?:\[(.*?)\])? to capture optional [text]
-		content = content.replace(/\\(cite|citeauthor|citeauthorlastnameand|citetitle|citeyear|citealternativetitle|citeurl)(?:\[(.*?)\])?\{(.+?)\}/g, (match, type, manualText, key) => {
+		// Now also handles comma-separated keys like \cite{key1, key2, key3}
+		content = content.replace(/\\(cite|citeauthor|citeauthorlastnameand|citetitle|citeyear|citealternativetitle|citeurl)(?:\[(.*?)\])?\{([^}]+)\}/g, (match, type, manualText, keysStr) => {
+			const keys = keysStr.split(/\s*,\s*/);
+			const renderedKeys = keys.map((key) => {
 			const isDuplicate = citedInThisBlock.has(key);
 			const instanceId = `ref-${key}-${Math.random().toString(36).substr(2, 5)}`;
 			const data = trackCitation(key, instanceId, isDuplicate);
@@ -872,6 +875,81 @@ function bibtexify() {
 					default:           linkText = `[${data.author}, ${data.year}]`;
 				}
 			}
+			return { key, linkText, data };
+			});
+
+			const html = renderedKeys.map(({ key, linkText, data }) => {
+				// Source icon as its own link (sibling, not nested) opening the source URL in a new tab
+				const svgIcon = data.url
+					? `<a class="bibtexify_auto_link_icon" href="${data.url}" target="_blank" rel="noopener noreferrer" title="View source"><span class="external_link_icon">
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://inksopdi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.namingofcopes/inkscape"
+   viewBox="-256 -256 1850 1850"
+   id="svg3025"
+   version="1.1"
+   inkscape:version="0.48.3.1 r988"
+   width="100%"
+   height="100%">
+  <metadata
+     id="metadata3035">
+    <rdf:RDF>
+      <cc:Work
+	 rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+	   rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs
+     id="defs3033" />
+  <sodipodi:namedview
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1"
+     objecttolerance="10"
+     gridtolerance="10"
+     guidetolerance="10"
+     inkscape:pageopacity="0"
+     inkscape:pageshadow="2"
+     inkscape:window-width="640"
+     inkscape:window-height="480"
+     id="namedview3031"
+     showgrid="false"
+     inkscape:zoom="0.13169643"
+     inkscape:cx="896"
+     inkscape:cy="896"
+     inkscape:window-x="0"
+     inkscape:window-y="25"
+     inkscape:window-maximized="0"
+     inkscape:current-layer="svg3025" />
+  <g
+     transform="matrix(1,0,0,-1,30.372881,1426.9492)"
+     id="g3027">
+    <path
+       d="M 1408,608 V 288 Q 1408,169 1323.5,84.5 1239,0 1120,0 H 288 Q 169,0 84.5,84.5 0,169 0,288 v 832 Q 0,1239 84.5,1323.5 169,1408 288,1408 h 704 q 14,0 23,-9 9,-9 9,-23 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 H 288 q -66,0 -113,-47 -47,-47 -47,-113 V 288 q 0,-66 47,-113 47,-47 113,-47 h 832 q 66,0 113 47 47,47 47,113 v 320 q 0,14 9 23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 384,864 V 960 q 0,-26 -19,-45 -19,-19 -45,-19 -26,0 -45,19 L 1507,1091 855,439 q -10,-10 -23,-10 -13,0 -23,10 L 695,553 q -10,10 -10,23 0,13 10,23 l 652,652 -176,176 q -19,19 -19,45 0,26 19,45 19,19 45,19 h 512 q 26,0 45,-19 19,-19 19,-45 z"
+       id="path3029"
+       inkscape:connector-curvature="0"
+       style="fill:currentColor" />
+  </g>
+</svg>
+</span></a>`
+					: "";
+
+				const instanceId = `ref-${key}-${Math.random().toString(36).substr(2, 5)}`;
+				const idAttribute = citedInThisBlock.has(key) ? "" : `id="${instanceId}"`;
+				const fullLink = `<a class="cite-stealth iframe-safe-link" ${idAttribute} data-target="bib-${key}" style="cursor:pointer;">${linkText}</a>`;
+				return `<span class="autociteelement">${fullLink}${svgIcon}</span>`;
+			}).join('');
+
+			return html;
+		});
 
 			// Source icon as its own link (sibling, not nested) opening the source URL in a new tab
 			const svgIcon = data.url
